abc7e69b5d45fdd32ba7c36e4639a104e7c3459b
Aaron Cherian <aaron.cherian2@gmail.com>
2021-07-13 18:57:49 -0400

    DLC config path GUI added

---
+++ freemocap_test.py
@@ -0,0 +1,14 @@
+import freemocap
+
+# sessionID = ''
+# stage = 1
+# useOpenPose = True
+# useMediaPipe = False
+# useDLC = False
+# debug = False
+
+
+# freemocap.RunMe(sessionID,stage,useOpenPose,useMediaPipe,useDLC,debug)
+
+freemocap.RunMe( stage = 1, useDLC=True,setDataPath=False)
+

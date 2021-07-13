3bc8ed4ad4aa0b7eb7704bca10fe0733a48feaaf
Aaron Cherian <aaron.cherian2@gmail.com>
2021-07-13 18:57:49 -0400

    DLC config path GUI added

--- freemocap/recordingconfig.py
+++ freemocap/recordingconfig.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 folder_setup = ['RawVideos','SyncedVideos','CalVideos','DataArrays','DLCData','OpenPoseData','MediaPipeData','imOut']
 
-default_parameters = {'path_to_save': str(Path.cwd()),'dlcconfig_paths':[], 'rotations':{},'parameters':{'exposure':-5,'resWidth':640,'resHeight':480,'framerate':25,'codec':'DIVX'}}
+default_parameters = {'path_to_save': str(Path.cwd()),'dlc_config_paths':[], 'rotations':{},'parameters':{'exposure':-5,'resWidth':640,'resHeight':480,'framerate':25,'codec':'DIVX'}}
 saved_parameters = default_parameters
 parameters_for_yaml = {'default':default_parameters,'saved':{}}
 

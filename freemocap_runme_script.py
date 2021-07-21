b73a4718d2a4a923110f97d0fdf6b4f18e1bb99d
jon matthis <jonmatthis@gmail.com>
2021-07-21 12:37:39 -0400

    2021-07-21 Reddit/Twitter post

--- freemocap_runme_script.py
+++ freemocap_runme_script.py
@@ -4,7 +4,7 @@ import socket
 
 dlcConfigPath = Path(r"C:\Users\jonma\Dropbox\DLC_Models\PinkGreenRedJugglingBalls-JSM-2021-05-31\config.yaml")
 
-# if socket.gethostname() == 'DESKTOP-DCG6K4F':
-#      fmc.RunMe(sessionID = 'sesh_21-07-08_131030', stage=7, openPoseDummyRun=True, useDLC = True, dlcConfigPath=dlcConfigPath) 
-# else:
-fmc.RunMe(stage =7, useDLC = True, dlcConfigPath=dlcConfigPath, recordVid = False) 
+if socket.gethostname() == 'DESKTOP-DCG6K4F':
+     fmc.RunMe( stage=7, openPoseDummyRun=True, useDLC = True, dlcConfigPath=dlcConfigPath, recordVid = True) 
+else:
+    fmc.RunMe( useDLC = True, dlcConfigPath=dlcConfigPath, recordVid = True) 

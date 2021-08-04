978b8916fb23f7a0e8e7ab28adb4cdc424ba82c6
Aaron Cherian <aaron.cherian2@gmail.com>
2021-08-04 18:25:59 -0400

    PYQT mediapipe fixes

--- freemocap_runme_script.py
+++ freemocap_runme_script.py
@@ -2,9 +2,11 @@ import freemocap as fmc
 from pathlib import Path
 import socket 
 
-dlcConfigPath = Path(r"C:\Users\jonma\Dropbox\DLC_Models\PinkGreenRedJugglingBalls-JSM-2021-05-31\config.yaml")
+# dlcConfigPath = Path(r"C:\Users\jonma\Dropbox\DLC_Models\PinkGreenRedJugglingBalls-JSM-2021-05-31\config.yaml")
 
-if socket.gethostname() == 'DESKTOP-DCG6K4F':
-     fmc.RunMe( stage=7, openPoseDummyRun=True, useDLC = True, dlcConfigPath=dlcConfigPath, recordVid = True) 
-else:
-    fmc.RunMe(sessionID = 'sesh_21-07-29_112223',stage = 7,useOpenPose=True, useMediaPipe=True) 
+# if socket.gethostname() == 'DESKTOP-DCG6K4F':
+#      fmc.RunMe( stage=7, openPoseDummyRun=True, useDLC = True, dlcConfigPath=dlcConfigPath, recordVid = True) 
+# else:
+#     fmc.RunMe( useDLC = True, dlcConfigPath=dlcConfigPath, recordVid = True) 
+
+fmc.RunMe(sessionID = 'sesh_21-07-29_104227', stage = 6, useMediaPipe=True, useOpenPose=False, setDataPath = True)

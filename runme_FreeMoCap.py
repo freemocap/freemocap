e3dd96145be81e172ff2ffb4c41bddde688626c4
jonmatthis <jonmatthis@gmail.com>
2021-09-30 12:15:24 -0400

    Update runme_FreeMoCap.py

--- runme_FreeMoCap.py
+++ runme_FreeMoCap.py
@@ -3,12 +3,5 @@ import freemocap as fmc
 
 #fmc.RunMe(useOpenPose = False, useMediaPipe=True, stage = 5, sessionID ='sesh_2021-09-13_14_34_12', recordVid=False)
 
-fmc.RunDemo()
+fmc.RunMe()
 
-#fmc.RunMe()
-
-# word = 'data'
-
-# mystring = 'freemocap_data'
-# if word in mystring: 
-#    print('success')

6110b3b96cc80838a986cbc20e17cc08140e2259
aaroncherian <aaron.cherian2@gmail.com>
2021-10-14 17:19:45 -0400

    anipose fixes

--- runme_FreeMoCap.py
+++ runme_FreeMoCap.py
@@ -3,5 +3,5 @@ import freemocap as fmc
 
 #fmc.RunMe(useOpenPose = False, useMediaPipe=True, stage = 5, sessionID ='sesh_2021-09-13_14_34_12', recordVid=False)
 
-fmc.RunMe()
+fmc.RunMe(sessionID = 'sesh_2021-08-29_17_55_45',stage = 3)
 

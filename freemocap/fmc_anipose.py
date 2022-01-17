10f6fdb1f48355795309c24c1dfc65ae9d2ad595
Anatoli Babenia <anatoli@rainforce.org>
2022-01-17 16:26:22 +0300

    Fix scale_l0ength typo introduced in 07f55f80

--- freemocap/fmc_anipose.py
+++ freemocap/fmc_anipose.py
@@ -1252,7 +1252,7 @@ class CameraGroup:
             lengths = np.linalg.norm(p3ds[:, a] - p3ds[:, b], axis=1)
             expected = joint_lengths[cix]
             errors_lengths[cix] = 100*(lengths - expected)/expected
-        errors_lengths = errors_lengths.ravel() * scale_l0ength
+        errors_lengths = errors_lengths.ravel() * scale_length
 
         errors_lengths_weak = np.empty((n_constraints_weak, n_frames), dtype='float64')
         for cix, (a, b) in enumerate(constraints_weak):

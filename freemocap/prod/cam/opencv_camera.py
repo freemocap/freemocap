23d1cf3826d14cf146720804dc2a3df6cef61df8
Endurance Idehen <jeidehen@gmail.com>
2022-01-25 19:38:35 -0600

    comitting current state

--- freemocap/prod/cam/opencv_camera.py
+++ freemocap/prod/cam/opencv_camera.py
@@ -1,6 +1,8 @@
 import cv2
 from pydantic import BaseModel
 
+from freemocap.prod.cam.cam_detection import DetectPossibleCameras
+
 
 class OpenCVCameraOptions(BaseModel):
     port_number: int
@@ -10,11 +12,13 @@ class OpenCVCameraOptions(BaseModel):
 
 
 # # OpenCV Implementation of interacting with a camera
-# class OpenCVCamera:
-#
-#     def __init__(self, options: OpenCVCameraOptions):
-#         self._options = options
-#
-#     def connect(self):
-#         # c = cv2.VideoCapture()
-#         cv2.VideoCapture(, cv2.CAP_ANY)
+class OpenCVCamera:
+
+    def __init__(self, options: OpenCVCameraOptions):
+        self._options = options
+
+    def connect(self):
+        c = DetectPossibleCameras()
+        available_cameras = c.find_available_cameras()
+
+

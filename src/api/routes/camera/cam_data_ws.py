82ed0031b0ff97268b9567e556b6bfcd2ce295fe
Endurance Idehen <jeidehen@gmail.com>
2022-02-16 16:14:57 -0600

    should work

--- src/api/routes/camera/cam_data_ws.py
+++ src/api/routes/camera/cam_data_ws.py
@@ -2,6 +2,7 @@ import base64
 import logging
 
 import cv2
+import orjson
 from fastapi import APIRouter, WebSocket
 
 from jon_scratch.opencv_camera import OpenCVCamera
@@ -13,6 +14,8 @@ cam_ws_router = APIRouter()
 @cam_ws_router.websocket("/ws/{webcam_id}")
 async def websocket_endpoint(websocket: WebSocket, webcam_id: str):
     await websocket.accept()
+    # TODO: Consider Spawning a new Process here - alleviate main thread issues
+    # We could spawn a new Process here directlu
     cam = OpenCVCamera(port_number=webcam_id)
     cam.connect()
 
@@ -26,7 +29,13 @@ async def websocket_endpoint(websocket: WebSocket, webcam_id: str):
             success, frame = cv2.imencode('.png', image)
             if not success:
                 continue
-            await websocket.send_bytes(base64.b64encode(frame.tobytes()))
+            d = {
+                "frameData": str(frame.tobytes()),
+                # "frameData": base64.b64encode(frame.tobytes()),
+                "timestamp": timestamp,
+            }
+            d = orjson.dumps(d)
+            await websocket.send_bytes(d)
     except:
         logger.info(f"Camera {webcam_id} is now closed.")
         cam.close()

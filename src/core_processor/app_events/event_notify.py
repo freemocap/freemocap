853d7bf787d887f5067c1c6f66eae0eab6c68f83
Endurance Idehen <jeidehen@gmail.com>
2022-02-16 23:50:39 -0600

    multiprocessing over asyncio for websocket connection

--- src/core_processor/app_events/event_notify.py
+++ src/core_processor/app_events/event_notify.py
@@ -1,6 +1,7 @@
+import asyncio
 import logging
+import multiprocessing
 
-from src.core_processor.app_events.app_queue import AppQueue
 from src.core_processor.subscribers.subscribers import subscribers
 
 logger = logging.getLogger(__name__)
@@ -9,22 +10,18 @@ logger = logging.getLogger(__name__)
 class EventNotifier:
 
     def __init__(self, webcam_ids):
-        self._app_queues = AppQueue()
-        self._workers = []
+        self._workers = self._construct()
         self._webcam_ids = webcam_ids
-        self._construct()
+
+    async def notify_all_subscribers(self, queue: multiprocessing.Queue):
+        while True:
+            try:
+                message = queue.get(timeout=1)
+                for worker in self._workers:
+                    await worker.process(message)
+            except Exception as e:
+                print(e)
+                logger.error("Notifier dead")
 
     def _construct(self):
-        self._workers = [sub() for sub in subscribers]
-
-    async def notify_all_subscribers(self):
-        while True:
-            for webcam_id in self._webcam_ids:
-                queue = self._app_queues.get_by_webcam_id(webcam_id)
-                try:
-                    message = await queue.get()
-                    for worker in self._workers:
-                        await worker.process(message)
-                except Exception as e:
-                    logger.error(e)
-                    logger.error("Notifier dead")
+        return [sub() for sub in subscribers]

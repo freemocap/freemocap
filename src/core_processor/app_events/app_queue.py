896743fa483d990ad8ef2b66399e92e8db392293
Endurance Idehen <jeidehen@gmail.com>
2022-02-18 07:29:22 -0600

    pass all queues into board detection routine

--- src/core_processor/app_events/app_queue.py
+++ src/core_processor/app_events/app_queue.py
@@ -1,12 +1,25 @@
 import multiprocessing
-from typing import List
-# from faster_fifo import Queue
+from multiprocessing import Queue
+from typing import Dict, List, Optional
+
+from aiomultiprocess.core import get_manager
+
+
+class CameraFrameQueue:
+    webcam_id: str
+    queue: Optional[Queue]
+
+    def __init__(self, webcam_id: str, queue: Queue):
+        self.webcam_id = webcam_id
+        self.queue = queue
 
 
 class AppQueue:
-    _queues = {}
+    _queues: Dict[str, CameraFrameQueue] = {}
 
-    def __init__(self, manager: multiprocessing.Manager):
+    def __init__(self, manager: multiprocessing.Manager = None):
+        if not manager:
+            manager = get_manager()
         self._manager: multiprocessing.Manager = manager
 
     def create_all(self, webcam_ids: List[str]):
@@ -15,11 +28,17 @@ class AppQueue:
 
     def create(self, webcam_id: str):
         if webcam_id not in self._queues:
-            self._queues[webcam_id] = {"queue": self._manager.Queue()}
+            self._queues[webcam_id] = CameraFrameQueue(
+                webcam_id=webcam_id,
+                queue=self._manager.Queue()
+            )
 
     def get_by_webcam_id(self, webcam_id: str):
-        return self._queues[webcam_id]["queue"]
+        return self._queues[webcam_id].queue
 
+    @property
+    def queues(self):
+        return self._queues
 
 def create_or_get_queue():
     pass
--- src/core_processor/board_detection/detect.py
+++ src/core_processor/board_detection/detect.py
@@ -1,25 +1,30 @@
 import logging
+from typing import Dict
 
 import cv2
 from aiomultiprocess import Process
+
+from src.core_processor.app_events.app_queue import CameraFrameQueue
 
 logger = logging.getLogger(__name__)
 
 
 class BoardDetection:
 
-    async def create_new_process_for_run(self, queue):
-        p = Process(target=self.process, args=(queue,))
+    async def create_new_process_for_run(self, queues: Dict[str, CameraFrameQueue]):
+        p = Process(target=self.process, args=(queues,))
         p.start()
         await p.join()
 
-    async def process(self, queue):
+    async def process(self, queues: Dict[str, CameraFrameQueue]):
         while True:
-            try:
-                image, timestamp = queue.get(timeout=.01)
-                cv2.imshow('blah', image)
-                exit_key = cv2.waitKey(1)
-                if exit_key == 27:
-                    break
-            except:
-                pass
+            for webcam_id in queues:
+                queue = queues[webcam_id].queue
+                try:
+                    image, timestamp = queue.get(timeout=.01)
+                    cv2.imshow(webcam_id, image)
+                    exit_key = cv2.waitKey(1)
+                    if exit_key == 27:
+                        break
+                except:
+                    pass

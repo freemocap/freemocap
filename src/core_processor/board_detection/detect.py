4a8d51d01d457033f6c0e0e4ce16a5cc49c70119
Endurance Idehen <jeidehen@gmail.com>
2022-02-21 16:31:53 -0600

    add black for autocommit

--- src/core_processor/board_detection/detect.py
+++ src/core_processor/board_detection/detect.py
@@ -5,13 +5,13 @@ import cv2
 import numpy as np
 
 from src.core_processor.board_detection.base_pose_estimation import detect_charuco_board
-from src.core_processor.board_detection.charuco_image_annotator import \
-    annotate_image_with_charuco_data
+from src.core_processor.board_detection.charuco_image_annotator import (
+    annotate_image_with_charuco_data,
+)
 from src.core_processor.processor import create_opencv_cams
 
 
 class BoardDetection:
-
     async def process(self):
         logger = logging.getLogger(__name__)
         cv_cams = create_opencv_cams()
@@ -30,21 +30,30 @@ class BoardDetection:
 
                 if frame is None:
                     continue
-
                 port_number = str(cv_cam.port_number)
-                print(f'got image of shape {frame.shape} from camera at port {port_number}')
-                charuco_corners, charuco_ids, aruco_square_corners, aruco_square_ids = \
-                    detect_charuco_board(frame)
+                print(
+                    f"got image of shape {frame.shape} from camera at port {port_number}"
+                )
+                (
+                    charuco_corners,
+                    charuco_ids,
+                    aruco_square_corners,
+                    aruco_square_ids,
+                ) = detect_charuco_board(frame)
                 # TODO - Pull out timestamps per frame and calculate fps to display on image
-                success_bool = annotate_image_with_charuco_data(frame, port_number,
-                    charuco_corners, charuco_ids)
+                success_bool = annotate_image_with_charuco_data(
+                    frame, port_number, charuco_corners, charuco_ids
+                )
 
-                cv2.polylines(frame, np.int32([charuco_corners]), True, (0, 100, 255), 2)
+                cv2.polylines(
+                    frame, np.int32([charuco_corners]), True, (0, 100, 255), 2
+                )
 
                 cv2.imshow(port_number, frame)
                 exit_key = cv2.waitKey(1)
                 if exit_key == 27:
                     break
+
 
 if __name__ == "__main__":
     ## Jon, Run me to easily start a charuco board detect run

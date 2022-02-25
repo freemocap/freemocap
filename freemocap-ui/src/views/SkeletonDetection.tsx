dd292c89198020188c80afa0957c67297cddda55
Endurance Idehen <jeidehen@gmail.com>
2022-02-25 16:13:07 -0600

    frame-capture mediapip

--- freemocap-ui/src/views/SkeletonDetection.tsx
+++ freemocap-ui/src/views/SkeletonDetection.tsx
@@ -4,7 +4,6 @@ import {CaptureType, useFrameCapture} from "../services/frame-capture";
 
 export const SkeletonDetection = () => {
   const [frameCapture, data] = useFrameCapture("0", CaptureType.SkeletonDetection);
-
   if (!data) {
     return null;
   }

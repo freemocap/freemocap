c2444cf79be47fa27e1318d8e82e784496801324
Jon <jonmatthis@gmai.com>
2022-03-21 08:51:41 -0400

    leaving lens calibration there for now
    still needs work, but it'll do for the time being 
    
    - need add multi-camera support
    - the distorted/undistorted plots don't work for some reason
    - need to work on the `is_calibration_valid` sub-methods
    
    - change up the ransac thing - maybe only save 'best' calibrations and add `jittered' versions of those? 
    -maybe constrain values of lens distortion coeffs to 'reasonable' range (whatever that is?)

--- src/core_processor/camera_calibration/calibration_diagnostics_visualizer.py
+++ src/core_processor/camera_calibration/calibration_diagnostics_visualizer.py
@@ -2,13 +2,17 @@ import numpy as np
 import pyqtgraph as pg
 import cv2
 
+
 class CalibrationDiagnosticsVisualizer:
     def __init__(self):
         self.graphics_layout_window = pg.GraphicsLayoutWidget(show=True)
         self.graphics_layout_window.setWindowTitle('Reprojection Error')
 
+        # dummy data to initialize subplots
         self.reprojection_error_all = np.empty(0)
         self.reprojection_error_current_best = np.empty(0)
+        self.image_point_original = np.empty(0)
+        self.image_point_remapped = np.empty(0)
 
         # create image subplot (based on `GraphicsItems - ImageItem - video` example `python -m pyqtgraph.examples`)
         self.undistorted_image_subplot = self.graphics_layout_window.addViewBox()
@@ -16,28 +20,56 @@ class CalibrationDiagnosticsVisualizer:
         self.image_item_holder = pg.ImageItem(border='w')
         self.undistorted_image_subplot.addItem(self.image_item_holder)
 
-        # create reprojection error sublot
+        # create reprojection error subplot
         self.reprojection_error_subplot = self.graphics_layout_window.addPlot()
         self.reprojection_error_subplot.setLabel('left', "Reprojection Error", units='pixels')
         self.reprojection_error_subplot.setLabel('bottom', "Time", units='Frame#')
         self.reprojection_error_subplot.setYRange(0, 2)
-
+        self.reprojection_error_subplot.addLegend()
         self.reprojection_error_all_line = self.reprojection_error_subplot.plot(self.reprojection_error_all,
                                                                                 pen=(255, 0, 128),
                                                                                 name="reprojection error- all")
+        self.reprojection_error_current_best_line = self.reprojection_error_subplot.plot(
+            self.reprojection_error_current_best,
+            name="current best")
 
-        self.reprojection_error_current_best_line = self.reprojection_error_subplot.plot(self.reprojection_error_current_best)
+        # make next row
+        self.graphics_layout_window.nextRow()
+
+        # create lens distortion remapping subplot
+        self.image_point_remapping_subplot = self.graphics_layout_window.addPlot()
+        self.image_point_remapping_subplot.setLabel('left', "Y-position", units='pixels')
+        self.image_point_remapping_subplot.setLabel('bottom', "X-position", units='pixels')
+        self.image_point_remapping_subplot.addLegend()
+
+        self.image_point_original_line = self.image_point_remapping_subplot.plot(self.image_point_original,
+                                                                                 pen=(255, 200, 200),
+                                                                                 name="image points - original")
+        self.image_point_remapped_line = self.image_point_remapping_subplot.plot(self.image_point_remapped,
+                                                                                 pen=(0, 255, 128),
+                                                                                 name="image points - remapped")
+
+
+
+
+    def update_image_subplot(self, new_image):
+        self.image_item_holder.setImage(cv2.cvtColor(cv2.rotate(new_image, cv2.ROTATE_90_CLOCKWISE), cv2.COLOR_BGR2RGB))
 
     def update_reprojection_error_subplot(self, reprojection_error_new, reprojection_error_current_best):
-        # update reprojection error subplot
         self.reprojection_error_all = np.append(self.reprojection_error_all, reprojection_error_new)
-        self.reprojection_error_current_best = np.append(self.reprojection_error_current_best, reprojection_error_current_best)
+        self.reprojection_error_current_best = np.append(self.reprojection_error_current_best,
+                                                         reprojection_error_current_best)
 
         self.reprojection_error_all_line.setData(self.reprojection_error_all)
         self.reprojection_error_current_best_line.setData(self.reprojection_error_current_best)
 
-    def update_image_subplot(self, new_image):
-        self.image_item_holder.setImage(cv2.cvtColor(cv2.rotate(new_image, cv2.ROTATE_90_CLOCKWISE), cv2.COLOR_BGR2RGB))
+    def update_image_point_remapping_subplot(self, image_point_original_x, image_point_original_y,
+                                             image_point_remapped_x, image_point_remapped_y):
+        self.image_point_original_line.setData(x=image_point_original_x,
+                                               y=image_point_original_y)
+
+        self.image_point_original_line.setData(x=image_point_remapped_x,
+                                               y=image_point_remapped_y)
 
     def close(self):
         self.graphics_layout_window.close()

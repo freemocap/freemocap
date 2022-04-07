ec7942e9bccbcd9246af021ceb8263481b136baf
Jon <jonmatthis@gmai.com>
2022-04-07 17:57:15 -0400

    fiddling with timestamps

--- src/core_processor/fps/timestamp_manager.py
+++ src/core_processor/fps/timestamp_manager.py
@@ -1,15 +1,13 @@
-import time
 from typing import Dict, List
 
 import numpy as np
 
 
-class FPSCounter:
+class TimestampLogger:
     _num_frames_processed: int
     _start_time: float
 
     def __init__(self):
-        self._start_time = 0
         self._num_frames_processed = 0
         self._timestamps = np.empty(0)
 
@@ -22,24 +20,38 @@ class FPSCounter:
             return 0
         return np.nanmedian(np.diff(self._timestamps)/1e9)**-1
 
-    def elapsed(self):
-        return time.time() - self._start_time
+    @property
+    def number_of_frames(self):
+        return self._num_frames_processed
+
+    @property
+    def timestamps(self):
+        return self._timestamps
+
+    @property
+    def last_timestamp(self):
+        if self._timestamps.shape[0] > 0:
+            return self._timestamps[-1]
+        return None
 
     def increment_frame_processed(self, timestamp):
         self._timestamps = np.append(self._timestamps, timestamp)
         self._num_frames_processed += 1
 
 
-class FPSCamCounter:
+class TimestampManager:
     def __init__(self, webcam_ids: List[str]):
         self._webcam_ids = webcam_ids
-        self._counters: Dict[str, FPSCounter] = self._init_counters(webcam_ids)
+        self._counters: Dict[str, TimestampLogger] = self._init_counters(webcam_ids)
 
     def _init_counters(self, webcam_ids: List[str]):
         d = {}
         for webcam_id in webcam_ids:
-            d[webcam_id] = FPSCounter()
+            d[webcam_id] = TimestampLogger()
         return d
+
+    def get_timestamps_from_camera(self, webcam_id):
+        return self._counters[webcam_id]
 
     def increment_frame_processed_for(self, webcam_id, timestamp):
         self._counters[webcam_id].increment_frame_processed(timestamp)

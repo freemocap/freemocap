3657ad9d260ed382d190a7e7cb4f7afd843c752b
aaroncherian <aaron.cherian2@gmail.com>
2022-05-12 14:32:26 -0400

    Update freemocap_runme_script.py

--- freemocap_runme_script.py
+++ freemocap_runme_script.py
@@ -5,4 +5,4 @@ import freemocap as fmc
 
 
 
-fmc.RunMe(charucoSquareSize=73, sessionID= 'sesh_2022-05-09_15_40_59', stage = 5, useBlender=True, setDataPath=False, runMediaPipe=False, place_skeleton_on_origin=True)
+fmc.RunMe()

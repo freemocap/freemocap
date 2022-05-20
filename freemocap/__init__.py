27307543445d99dd646714718cd99fe4690e0187
Jon <jonmatthis@gmai.com>
2022-05-20 18:31:55 -0400

    fix __init__ bug

--- freemocap/__init__.py
+++ freemocap/__init__.py
@@ -8,34 +8,34 @@ from freemocap import fmc_demo, fmc_runme
 
 # from freemocap import fmc_runme, fmc_demo #JSM NOTE - there's gotta be a better way to do this lol
 
-# def RunMe(sessionID=None,
-#             stage=1,
-#             useOpenPose=True, 
-#             openPoseDummyRun=False,
-#             useMediaPipe=False,
-#             useDLC=False,
-#             dlcConfigPath=None,
-#             debug=False,
-#             setDataPath=False,
-#             userDataPath = None, 
-#             recordVid=False,
-#             reconstructionConfidenceThreshold = .8,
-#             charucoSquareSize = 36,
-#             ):
-#     """ 
-#     Passes user-set values from the freemocap_runme_script to the fmc_runme.Run function (and allows the user to run the pipeline with just an fmc.RunMe command)
-#     """  
-#     fmc_runme.Run(sessionID,
-#     stage,
-#     useOpenPose,
-#     openPoseDummyRun, 
-#     useMediaPipe,
-#     useDLC,
-#     dlcConfigPath,
-#     debug,
-#     setDataPath,
-#     userDataPath,
-#     recordVid)
+def RunMe(sessionID=None,
+            stage=1,
+            useOpenPose=True, 
+            openPoseDummyRun=False,
+            useMediaPipe=False,
+            useDLC=False,
+            dlcConfigPath=None,
+            debug=False,
+            setDataPath=False,
+            userDataPath = None, 
+            recordVid=False,
+            reconstructionConfidenceThreshold = .8,
+            charucoSquareSize = 36,
+            ):
+    """ 
+    Passes user-set values from the freemocap_runme_script to the fmc_runme.Run function (and allows the user to run the pipeline with just an fmc.RunMe command)
+    """  
+    fmc_runme.Run(sessionID,
+    stage,
+    useOpenPose,
+    openPoseDummyRun, 
+    useMediaPipe,
+    useDLC,
+    dlcConfigPath,
+    debug,
+    setDataPath,
+    userDataPath,
+    recordVid)
 
 def RunDemo():
     """ 

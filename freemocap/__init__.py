5ab05ed6d4cea53f8223894fc6b63721637249aa
Jon <jonmatthis@gmai.com>
2022-05-20 18:33:24 -0400

    ...

--- freemocap/__init__.py
+++ freemocap/__init__.py
@@ -1,41 +1,41 @@
 from ._version import get_versions
 
-__version__ = get_versions()["version"]
-del get_versions
+# __version__ = get_versions()["version"]
+# del get_versions
 
 from freemocap.fmc_runme import RunMe
 from freemocap import fmc_demo, fmc_runme
 
 # from freemocap import fmc_runme, fmc_demo #JSM NOTE - there's gotta be a better way to do this lol
 
-def RunMe(sessionID=None,
-            stage=1,
-            useOpenPose=True, 
-            openPoseDummyRun=False,
-            useMediaPipe=False,
-            useDLC=False,
-            dlcConfigPath=None,
-            debug=False,
-            setDataPath=False,
-            userDataPath = None, 
-            recordVid=False,
-            reconstructionConfidenceThreshold = .8,
-            charucoSquareSize = 36,
-            ):
-    """ 
-    Passes user-set values from the freemocap_runme_script to the fmc_runme.Run function (and allows the user to run the pipeline with just an fmc.RunMe command)
-    """  
-    fmc_runme.Run(sessionID,
-    stage,
-    useOpenPose,
-    openPoseDummyRun, 
-    useMediaPipe,
-    useDLC,
-    dlcConfigPath,
-    debug,
-    setDataPath,
-    userDataPath,
-    recordVid)
+# def RunMe(sessionID=None,
+#             stage=1,
+#             useOpenPose=True, 
+#             openPoseDummyRun=False,
+#             useMediaPipe=False,
+#             useDLC=False,
+#             dlcConfigPath=None,
+#             debug=False,
+#             setDataPath=False,
+#             userDataPath = None, 
+#             recordVid=False,
+#             reconstructionConfidenceThreshold = .8,
+#             charucoSquareSize = 36,
+#             ):
+#     """ 
+#     Passes user-set values from the freemocap_runme_script to the fmc_runme.Run function (and allows the user to run the pipeline with just an fmc.RunMe command)
+#     """  
+#     fmc_runme.Run(sessionID,
+#     stage,
+#     useOpenPose,
+#     openPoseDummyRun, 
+#     useMediaPipe,
+#     useDLC,
+#     dlcConfigPath,
+#     debug,
+#     setDataPath,
+#     userDataPath,
+#     recordVid)
 
 def RunDemo():
     """ 

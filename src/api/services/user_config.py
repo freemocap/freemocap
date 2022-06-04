d95e4ef88ed6ae37770de7dad7c6270d45da8ae2
Jon <jonmatthis@gmai.com>
2022-06-04 17:05:15 -0400

    still making camera preview routes

--- src/api/services/user_config.py
+++ src/api/services/user_config.py
@@ -20,19 +20,24 @@ def get_camera_name(camera_id: str):
 class UserConfigService:
     """take things like WebcamConfigModel and save it to a text file or whatever"""
 
-    def save_webcam_config_to_disk(self, webcam_config_model: WebcamConfigModel, session_id:str):
-        webcam_config_json = webcam_config_model.dict()
+    def save_webcam_config_to_disk(self, webcam_config_model: WebcamConfigModel, session_id: str):
+        webcam_config_json = webcam_config_model.json()
         camera_name = get_camera_name(webcam_config_model.webcam_id)
         json_file_name = camera_name + '_config.json'
-        webcam_config_json_path = Path(webcam_config_model.session_path) / json_file_name
+        session_path = get_session_path(session_id)
+        webcam_config_json_path = Path(session_path) / json_file_name
         with open(webcam_config_json_path, 'w') as outfile:
             outfile.write(str(webcam_config_json))
         return webcam_config_json
 
-    def webcam_config_by_id(self, webcam_id: str, session_id: str)->WebcamConfigModel:
+    def webcam_config_by_id(self, webcam_id: str, session_id: str) -> WebcamConfigModel:
+        print(f"webcam id: {webcam_id}")
         session_path = get_session_path(session_id)
         camera_name = get_camera_name(webcam_id)
+        print(f"webcam id (again): {webcam_id}")
+        print(f"camera name: {camera_name}")
         json_file_name = camera_name + '_config.json'
         webcam_config_json_path = Path(session_path) / json_file_name
+        print(f'webcam_config_path: {webcam_config_json_path}')
         webcam_config_model = WebcamConfigModel.parse_file(webcam_config_json_path)
         return webcam_config_model

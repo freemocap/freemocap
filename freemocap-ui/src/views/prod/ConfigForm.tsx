e58e15a2d05ad9d9397407f2e88f0796eb96c4db
Endurance Idehen <jeidehen@gmail.com>
2022-06-04 10:12:14 -0500

    Committing current set for api development

--- freemocap-ui/src/views/prod/ConfigForm.tsx
+++ freemocap-ui/src/views/prod/ConfigForm.tsx
@@ -1,14 +1,37 @@
-import { Typography } from "@mui/material"
+import {Typography, TextField, Button} from "@mui/material"
 import {Box} from "@mui/system";
+import {default as NumberFormat} from 'react-number-format';
 
+const ExposureNumberField = () => {
+  // TODO: -13 -> -1 (inclusive)
+  return (
+    <NumberFormat customInput={TextField} />
+  );
+}
 
 export const ConfigForm = () => {
   return (
     <Box>
 
-      <Typography>Exposure</Typography>
+      <Box>
+        <Typography>Exposure</Typography>
+        <ExposureNumberField />
+      </Box>
 
+      <Box>
+        <Typography>Resolution Width</Typography>
+        <NumberFormat customInput={TextField} />
 
+        <Typography>Resolution Height</Typography>
+        <NumberFormat customInput={TextField} />
+      </Box>
+
+      <Box>
+        <Typography>Image Rotation Degree</Typography>
+        <NumberFormat customInput={TextField} />
+      </Box>
+
+      <Button>Save Camera Settings</Button>
     </Box>
   )
 }

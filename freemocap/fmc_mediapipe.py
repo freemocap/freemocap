f853cf4fbbd7e3802de1b9052533ab807cd8811e
aaroncherian <aaron.cherian2@gmail.com>
2022-06-27 14:19:52 -0400

    adding frame finding/alignment

--- freemocap/fmc_mediapipe.py
+++ freemocap/fmc_mediapipe.py
@@ -12,7 +12,42 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
-
+mediapipe_indices = [
+    'nose',
+    'left_eye_inner',
+    'left_eye',
+    'left_eye_outer',
+    'right_eye_inner',
+    'right_eye',
+    'right_eye_outer',
+    'left_ear',
+    'right_ear',
+    'mouth_left',
+    'mouth_right',
+    'left_shoulder',
+    'right_shoulder',
+    'left_elbow',
+    'right_elbow',
+    'left_wrist',
+    'right_wrist',
+    'left_pinky',
+    'right_pinky',
+    'left_index',
+    'right_index',
+    'left_thumb',
+    'right_thumb',
+    'left_hip',
+    'right_hip',
+    'left_knee',
+    'right_knee',
+    'left_ankle',
+    'right_ankle',
+    'left_heel',
+    'right_heel',
+    'left_foot_index',
+    'right_foot_index'
+    ]
+    
 def runMediaPipe(session, dummyRun=False):
     """ 
     Run MediaPipe on synced videos, and save body tracking data to be parsed 

4e2619af37d4825058d1265b68e08cf7afc0980d
Jon <jonmatthis@gmai.com>
2022-07-05 21:42:44 -0400

    some tweaks to the workflow

--- src/freemocap_qt_gui/conference/shared_widgets/primary_button.py
+++ src/freemocap_qt_gui/conference/shared_widgets/primary_button.py
@@ -1,10 +1,8 @@
 from PyQt6.QtWidgets import QPushButton
 
 
-class PrimaryButton(QPushButton):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.setStyleSheet("""
+
+active_primary_button_style_sheet = """
         QPushButton {
             background-color: #336e6d;
             color: #ffffff;
@@ -17,4 +15,25 @@ class PrimaryButton(QPushButton):
         QPushButton:hover {
             background-color: #365d5f;
         }
-        """)
+        """
+
+not_active_primary_button_style_sheet = """
+        QPushButton {
+            background-color: 'lightgrey';
+            color: #ffffff;
+            border-radius: 4px;
+            min-height: 32px;
+            max-width: 160px;
+            font-weight: 400;
+            font-size: 16px;
+        }
+        QPushButton:hover {
+            background-color: 'lightgrey';
+        }
+        """
+
+class PrimaryButton(QPushButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setStyleSheet(active_primary_button_style_sheet)
+
--- src/freemocap_qt_gui/conference/workflows/show_cams_charuco.py
+++ src/freemocap_qt_gui/conference/workflows/show_cams_charuco.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QHBoxLayout, QLabel, QVBoxLayout, QWidget, QPushButton
+from PyQt6.QtWidgets import QHBoxLayout, QLabel, QVBoxLayout, QWidget, QPushButton, QCheckBox
 
 from src.cameras.save_synchronized_videos import save_synchronized_videos
 from src.freemocap_qt_gui.conference.workflows.single_camera import SingleCamera
@@ -14,6 +14,9 @@ class ShowCamsCharuco(QWidget):
         self._selected_cams = APP_STATE.selected_cameras
 
         self._continue_button = QPushButton("Continue")
+        self._continue_button.setEnabled(False)
+
+        self._use_previous_calibration_checkbox = self._create_use_previous_calibration_checkbox()
 
         container = QVBoxLayout()
 
@@ -32,10 +35,13 @@ class ShowCamsCharuco(QWidget):
         container.addLayout(title_layout)
         container.addLayout(video_stream_layout)
 
+        container.addWidget(self._use_previous_calibration_checkbox)
+
         #start/stop recording button layout
         record_button_layout = QHBoxLayout()
         self._start_recording_button = QPushButton('Begin Recording')
         self._stop_recording_button = QPushButton('Stop Recording')
+        self._stop_recording_button.setEnabled(False)
 
         self._start_recording_button.clicked.connect(self._start_recording_frames)
         self._stop_recording_button.clicked.connect(self._stop_recording_frames)
@@ -45,6 +51,7 @@ class ShowCamsCharuco(QWidget):
         container.addLayout(record_button_layout)
 
         container.addLayout(self._create_continue_button_container())
+        container.addWidget(self._use_previous_calibration_checkbox)
 
         self.setLayout(container)
 
@@ -52,16 +59,30 @@ class ShowCamsCharuco(QWidget):
     def continue_button(self):
         return self._continue_button
 
+    def _create_use_previous_calibration_checkbox(self):
+        previous_calibration_checkbox = QCheckBox("Use Previous Calibration")
+        previous_calibration_checkbox.setChecked(False)
+        previous_calibration_checkbox.stateChanged.connect(self._use_previous_calibration_changed)
+        return previous_calibration_checkbox
+
     def _create_continue_button_container(self):
         continue_button_layout = QHBoxLayout()
         continue_button_layout.addWidget(self._continue_button)
         return continue_button_layout
 
+    def _use_previous_calibration_changed(self):
+        self._continue_button.setEnabled(self._use_previous_calibration_checkbox.isChecked())
+        APP_STATE.use_previous_calibration = self._use_previous_calibration_checkbox.isChecked()
+
+
     def _start_recording_frames(self):
+        self._start_recording_button.setEnabled(False)
+        self._stop_recording_button.setEnabled(True)
         for cam in self._cam_widgets:
             cam.should_record_frames = True
 
     def _stop_recording_frames(self):
+        self._stop_recording_button.setEnabled(False)
         video_recorders = []
         for cam in self._cam_widgets:
             video_recorders.append(cam.video_recorder)
@@ -70,7 +91,7 @@ class ShowCamsCharuco(QWidget):
         save_synchronized_videos(video_recorders, calibration_videos=True)
         self._run_anipose_calibration()
         print("Multi-camera calibration complete!")
-
+        self._continue_button.setEnabled(True)
 
     def _run_anipose_calibration(self):
         print('Beginning Anipose calibration')

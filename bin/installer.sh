dfee1c176edcea1ef7f9ed7f6d9e866385ee60ff
Jon <jonmatthis@gmai.com>
2022-07-24 17:01:35 -0400

    `installer.sh` command works when copy-pasted into `powershell`, but `task installer` returns error ""`.` is not recognized..."

--- bin/installer.sh
+++ bin/installer.sh
@@ -1,2 +1,1 @@
-
-pyinstaller  --name="FreeMoCap"  --onefile  --hidden-import pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyqt6 --hidden-import pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyqt6 --hidden-import pyqtgraph.imageview.ImageViewTemplate_pyqt6  ./src/gui/main/main.py
+pyinstaller --name="FreeMoCap" --windowed --onefile --hidden-import pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyqt6 --hidden-import pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyqt6 --hidden-import pyqtgraph.imageview.ImageViewTemplate_pyqt6 ./src/gui/main/main.py

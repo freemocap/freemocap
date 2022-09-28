d2fc0c95980c6b3c20e48d0ec5503df965353f18
Jon <jonmatthis@gmai.com>
2022-09-28 07:08:02 -0400

    manually updating `diagnostic plots` stuff

--- src/cameras/create_timestamp_diagnostic_plots.py
+++ src/cameras/create_timestamp_diagnostic_plots.py
@@ -20,6 +20,7 @@ def gather_timestamps(list_of_frames: List[FramePayload]) -> np.ndarray:
 
 
 def create_timestamp_diagnostic_plots(
+    each_cam_raw_frame_list: List[List[FramePayload]],
     each_cam_synchronized_frame_list: List[List[FramePayload]],
     path_to_save_plots: Union[str, Path],
 ):
@@ -30,38 +31,73 @@ def create_timestamp_diagnostic_plots(
 
     plt.set_loglevel("warning")
 
-    each_cam_timestamps = []
+    each_cam_synchronized_timestamps = []
+
     for cam_frame_list in each_cam_synchronized_frame_list:
-        each_cam_timestamps.append(gather_timestamps(cam_frame_list))
+        each_cam_synchronized_timestamps.append(gather_timestamps(cam_frame_list))
+
+    each_cam_raw_timestamps = []
+    for cam_frame_list in each_cam_raw_frame_list:
+        each_cam_raw_timestamps.append(gather_timestamps(cam_frame_list))
 
     fig = plt.figure(figsize=(18, 10))
     max_frame_duration = 0.1
     ax1 = plt.subplot(
-        131,
-        title="Camera Frame Timestamp vs Frame#",
+        231,
+        title="(Raw) Camera Frame Timestamp vs Frame#",
         xlabel="Frame#",
         ylabel="Timestamp (sec)",
     )
     ax2 = plt.subplot(
-        132,
+        232,
         ylim=(0, max_frame_duration),
-        title="Camera Frame Duration Trace",
+        title="(Raw) Camera Frame Duration Trace",
         xlabel="Frame#",
         ylabel="Duration (sec)",
     )
     ax3 = plt.subplot(
-        133,
+        233,
         xlim=(0, max_frame_duration),
-        title="Camera Frame Duration Histogram (count)",
+        title="(Raw) Camera Frame Duration Histogram (count)",
+        xlabel="Duration(s, 1ms bins)",
+        ylabel="Probability",
+    )
+    ax4 = plt.subplot(
+        234,
+        title="(Synchronized) Camera Frame Timestamp vs Frame#",
+        xlabel="Frame#",
+        ylabel="Timestamp (sec)",
+    )
+    ax5 = plt.subplot(
+        235,
+        ylim=(0, max_frame_duration),
+        title="(Synchronized) Camera Frame Duration Trace",
+        xlabel="Frame#",
+        ylabel="Duration (sec)",
+    )
+    ax6 = plt.subplot(
+        236,
+        xlim=(0, max_frame_duration),
+        title="(Synchronized) Camera Frame Duration Histogram (count)",
         xlabel="Duration(s, 1ms bins)",
         ylabel="Probability",
     )
 
-    for camera_number, timestamps in enumerate(each_cam_timestamps):
+    for camera_number, timestamps in enumerate(each_cam_raw_timestamps):
         ax1.plot(timestamps, label=f"Camera# {str(camera_number)}")
         ax1.legend()
         ax2.plot(np.diff(timestamps), ".")
         ax3.hist(
+            np.diff(timestamps),
+            bins=np.arange(0, max_frame_duration, 0.0025),
+            alpha=0.5,
+        )
+
+    for camera_number, timestamps in enumerate(each_cam_synchronized_timestamps):
+        ax4.plot(timestamps, label=f"Camera# {str(camera_number)}")
+        ax4.legend()
+        ax5.plot(np.diff(timestamps), ".")
+        ax6.hist(
             np.diff(timestamps),
             bins=np.arange(0, max_frame_duration, 0.0025),
             alpha=0.5,

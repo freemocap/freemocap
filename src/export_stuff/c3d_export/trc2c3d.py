5c9efce2680872f5c8d30ba182b493eec62f521e
mprib <prible@gmail.com>
2022-10-01 11:33:12 -0500

    preparing for pull request

--- src/export_stuff/c3d_export/trc2c3d.py
+++ src/export_stuff/c3d_export/trc2c3d.py
@@ -1,23 +1,41 @@
-# %%
+# Primary function: `trc_to_c3d()`
+# 
+# Inputs: 
+#   - filepath to an existing trc file 
+#   - target c3d destination
+# 
+# Effect of function call: creates a c3d at the target destination.
+#
+# Note: This works on the sample mediapipe output of "interpolated.trc" and then 
+# viewing in within Visual3D, but I haven't otherwise tested it.
+#
+# Author: mprib
+ 
 import numpy as np
 from collections import defaultdict
+from pathlib import Path
 
 import c3d
-from pathlib import Path
 import re
 
 def parse_trc_header(trc_filepath):
     """
-    from a given .trc file, return the information that will be used
-    as inputs for creating the .c3d file header
+    Given a path to a .trc file, return the metadata that will be used
+    as inputs for creating the .c3d file header. These are the parameters
+    displayed on line 3 of the trc file format
+
+    Returns: Frame Rate, Frame Count, Start Frame, Units (string)
     """
 
-    line_number =0
+
     with open(Path(trc_filepath)) as trc:
+        line_number =0
+        
         for line in trc:
+            # trc is tab delimited
             parsed_line = re.split(r'\t', line)
 
-            # header parameters
+            # header parameters on third row
             if line_number == 2:
                 frame_rate = parsed_line[1]
                 frame_count = parsed_line[2]
@@ -31,17 +49,20 @@ def parse_trc_header(trc_filepath):
 
 def parse_trc_points(trc_filepath):
     """
-    returns: 
-    - a list of point names 
+    Given a path to a .trc, returns: 
+    - a list of point names
+        - a useful input for a c3d header 
     - a nested dictionary structured as frame:point:XYZ coord
+        - used to construct a nested array of [[X, Y, Z, 0, 0], [....]],...]
+          for each frame
 
-    To be used to create the point trajectories of a trc file
+    These outputs are to be used to create the point trajectories of a trc file
     """
 
     points_by_frame = defaultdict(lambda: {})
 
-    line_number =0
     with open(Path(trc_filepath)) as trc:
+        line_number =0
         for line in trc:
             parsed_line = re.split(r'\t', line)
             
@@ -54,62 +75,75 @@ def parse_trc_points(trc_filepath):
             if line_number >= 6:
                 # construct a list of coordinates for each of the points
                 frame_number = parsed_line[0]
-                print(frame_number)
 
-                # skip the frame number and time (first two columns)
+                # skip the frame number and timestamp (first two columns)
                 xyz_start = 2
                 for point in point_names:
 
-                    # take slices of size 3
-                    raw_array = parsed_line[xyz_start:xyz_start+3]
-                    print(point, raw_array)
+                    # take slices of size 3 to get X, Y, Z for each point
+                    raw_list = parsed_line[xyz_start:xyz_start+3]
 
                     # clean up new lines and convert to numeric
-                    raw_array = [float(i.replace("\n","")) for i in raw_array]
-                    # add in missing zeroes required by c3d
-                    raw_array.extend([0,0])
-                    # print("Raw Arrary Length:" + str(len(raw_array)))
-                    points_by_frame[frame_number][point] = raw_array
+                    raw_list = [float(i.replace("\n","")) for i in raw_list]
+
+                    # add in missing zeroes required by c3d, update dictionary
+                    raw_list.extend([0,0])
+                    points_by_frame[frame_number][point] = raw_list
                     xyz_start+=3
 
             line_number+=1
 
     return point_names, points_by_frame
 
+
+def trc_to_c3d(trc_filepath, c3d_filepath):
+    """
+    Given a path to a trc file, convert it into a c3d file format and save it out
+    to the provide c3d filepath.
+    """
     
+    # parse out individual components from trc using helper functions
+    frame_rate, frame_count, frame_start, units = parse_trc_header(trc_sample_path)
+    point_names, points_by_frame = parse_trc_points(trc_sample_path)
+
+    # Initialize writer object and associated metadata
+    writer = c3d.Writer()
+    writer._point_units= units
+
+    new_header = c3d.c3d.Header()
+    new_header.frame_rate = frame_rate
+    new_header.first_frame= frame_start
+    new_header.last_frame = frame_count - frame_start + 1
+    new_header.point_count = len(point_names)
+    writer._header = new_header
+    writer.set_start_frame(new_header.first_frame)
+
+    # convert point names to appropriate format
+    point_names = np.array(point_names, dtype=object)
+    writer.set_point_labels(point_names)
+
+    # single blank analog label, otherwise no file creation
+    writer.set_analog_labels(np.array([''], dtype=object))
+
+    for frame in points_by_frame.keys():
+        frame_points = []
+
+        for point in point_names:
+            frame_points.append(points_by_frame[str(frame)][point])
+
+        writer.add_frames((np.array(frame_points, dtype=np.float32), np.array([[]])))
+
+    with open(Path(c3d_filepath), 'wb') as h:
+        writer.write(h)
 # %%
-# return frame_rate, point_names
-trc_sample_path = r"C:\Users\Mac Prible\repos\freemocap\src\export_stuff\c3d_export\dao_yin_interpolated.trc"
-frame_rate, frame_count, frame_start, units = parse_trc_header(trc_sample_path)
-point_names, points_by_frame = parse_trc_points(trc_sample_path)
+# Test Functionality
+if __name__ == "__main__":
+    # note this file has a different vertical orientation than v3d, so it will
+    # need to be rotated around to view the marker trajectories
+    # import sys
 
-# Use variables from parse function to create new c3d file
+    current_folder = Path(__file__).parent
+    trc_sample_path = Path(current_folder,"interpolated.trc")
+    new_c3d = Path(current_folder,"test_c3d_export.c3d")
+    trc_to_c3d(trc_sample_path, new_c3d)
 # %%
-writer = c3d.Writer()
-writer._point_units= units
-
-new_header = c3d.c3d.Header()
-new_header.frame_rate = frame_rate
-new_header.first_frame= frame_start
-new_header.last_frame = frame_count - frame_start + 1
-new_header.point_count = len(point_names)
-writer._header = new_header
-writer.set_start_frame(new_header.first_frame)
-
-# convert point names to appropriate format
-point_names = np.array(point_names, dtype=object)
-writer.set_point_labels(point_names)
-
-# single blank analog label, otherwise no file creation
-writer.set_analog_labels(np.array([''], dtype=object))
-
-for frame in points_by_frame.keys():
-    frame_points = []
-
-    for point in point_names:
-        frame_points.append(points_by_frame[str(frame)][point])
-
-    writer.add_frames((np.array(frame_points, dtype=np.float32), np.array([[]])))
-
-with open(Path("from_trc.c3d"), 'wb') as h:
-    writer.write(h)

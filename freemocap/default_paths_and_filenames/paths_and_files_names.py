9d30a310dc305a8049a3d52ba554ab0280a523f4
jonmatthis <jonmatthis@gmail.com>
2023-01-01 10:17:34 -0500

    live load css style sheet :D

---
+++ freemocap/default_paths_and_filenames/paths_and_files_names.py
@@ -0,0 +1,28 @@
+import logging
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+# directory names
+BASE_FREEMOCAP_DATA_FOLDER_NAME = "freemocap_data"
+
+
+def os_independent_home_dir():
+    return str(Path.home())
+
+
+def get_freemocap_data_folder_path(create_folder: bool = True):
+    freemocap_data_folder_path = Path(
+        os_independent_home_dir(), BASE_FREEMOCAP_DATA_FOLDER_NAME
+    )
+
+    if create_folder:
+        freemocap_data_folder_path.mkdir(exist_ok=create_folder, parents=True)
+
+    return str(freemocap_data_folder_path)
+
+
+def get_css_stylesheet_path():
+    return str(
+        Path(__file__).parent.parent / "qt_gui" / "style_sheet" / "qt_style_sheet.css"
+    )

91642866d0e00e2395a8aa2d6d0c35c73b0544a0
jonmatthis <jonmatthis@gmail.com>
2023-01-06 08:54:55 -0500

    added bare bones `export to blender` functionality :)

---
+++ freemocap/core_processes/visualization/blender_stuff/export_to_blender.py
@@ -0,0 +1,38 @@
+import logging
+from pathlib import Path
+from typing import Union
+
+from freemocap.core_processes.visualization.blender_stuff.create_blend_file_from_session_data import (
+    create_blend_file_from_session_data,
+)
+from freemocap.core_processes.visualization.blender_stuff.get_best_guess_of_blender_path import (
+    get_best_guess_of_blender_path,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def export_to_blender(
+    recording_folder_path: Union[str, Path],
+    blender_exe_path: Union[str, Path] = get_best_guess_of_blender_path(),
+):
+
+    blender_file_name = Path(recording_folder_path).stem + ".blend"
+    blender_file_path = Path(recording_folder_path) / blender_file_name
+    logger.info(
+        f"Exporting session data to a Blender scene at: {str(blender_file_path)} using Blender executable at {str(blender_exe_path)}"
+    )
+
+    create_blend_file_from_session_data(
+        recording_folder_path=recording_folder_path, blender_exe_path=blender_exe_path
+    )
+
+    return str(blender_file_path)
+
+
+if __name__ == "__main__":
+    from freemocap.configuration.paths_and_files_names import (
+        get_most_recent_recording_path,
+    )
+
+    export_to_blender(get_most_recent_recording_path())
---
+++ freemocap/core_processes/visualization/blender_stuff/get_best_guess_of_blender_path.py
@@ -0,0 +1,46 @@
+import logging
+import platform
+from pathlib import Path
+from typing import Union
+
+
+logger = logging.getLogger(__name__)
+
+
+def guess_blender_exe_path_from_path(base_path: Union[str, Path]) -> Path:
+    blender_folder_path = [path for path in base_path.glob("Blender*")]
+    if len(blender_folder_path) > 0:
+        blender_exe_path = blender_folder_path[0] / "blender.exe"
+        return blender_exe_path
+
+    return
+
+
+def get_best_guess_of_blender_path():
+    if platform.system() == "Windows":
+
+        base_path = Path("C:\Program Files\Blender Foundation")
+        blender_exe_path = guess_blender_exe_path_from_path(base_path)
+
+        if blender_exe_path is not None:
+            if not blender_exe_path.is_file():
+                base_path = Path(Path.home()) / "Blender Foundation"
+                blender_exe_path = guess_blender_exe_path_from_path(base_path)
+
+        if blender_exe_path is not None and blender_exe_path.is_file():
+            logger.info(
+                f"Windows machine detected - guessing that `blender` is installed at: {str(blender_exe_path)}"
+            )
+
+            return str(blender_exe_path)
+        else:
+            logger.warning(
+                f"Could not find `blender.exe` in the expected locations. Please located it manually (or install Blender, if it isn't installed)."
+            )
+            return None
+
+    else:
+        logger.info(
+            f"Non-Windows machine detected - TODO - Test how this works on Mac/Linux"
+        )
+        return None

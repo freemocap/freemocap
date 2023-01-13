c610b8a2d7f470c5a0c0fd3b7ebb152f03785a8a
jonmatthis <jonmatthis@gmail.com>
2023-01-13 09:15:55 -0500

    pyproj build working?

--- freemocap/__init__.py
+++ freemocap/__init__.py
@@ -6,6 +6,8 @@ __version__ = "v2023.01.1000"
 __description__ = (
     "A free and open source markerless motion capture system for everyone 💀✨"
 )
+
+
 __package_name__ = "freemocap"
 __repo_url__ = f"https://github.com/freemocap/{__package_name__}/"
 __repo_issues_url__ = f"{__repo_url__}issues"

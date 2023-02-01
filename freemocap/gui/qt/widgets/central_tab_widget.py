906651f47ce4ecaf1faf3b2ef979deeb602c72b3
jonmatthis <jonmatthis@gmail.com>
2023-02-01 11:29:47 -0500

    added `skelly_viewer` tab :D

--- freemocap/gui/qt/widgets/central_tab_widget.py
+++ freemocap/gui/qt/widgets/central_tab_widget.py
@@ -1,33 +1,39 @@
+import logging
+
+import skelly_viewer
+import skellycam
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QTabWidget, QVBoxLayout, QWidget, QGroupBox
+from skelly_viewer import SkellyViewer
 from skellycam import SkellyCamControllerWidget, SkellyCamViewerWidget
 
-from freemocap.gui.qt.widgets.welcome_tab_widget import (
-    WelcomeCreateOrLoadNewSessionPanel,
+from freemocap.gui.qt.widgets.welcome_panel_widget import (
+    WelcomeToFreemocapPanel,
 )
+
+logger = logging.getLogger(__name__)
 
 
 class CentralTabWidget(QTabWidget):
     def __init__(
         self,
-        camera_view_widget: SkellyCamViewerWidget,
-        camera_controller_layout: QWidget,
-        welcome_to_freemocap_widget: WelcomeCreateOrLoadNewSessionPanel,
-        visualize_data_widget,
+        skelly_cam_widget: SkellyCamViewerWidget,
+        camera_controller_widget: QWidget,
+        welcome_to_freemocap_widget: WelcomeToFreemocapPanel,
+        skelly_viewer_widget: SkellyViewer,
         parent=None,
     ):
-
         super().__init__(parent=parent)
         self.parent = parent
 
-        self._camera_view_widget = camera_view_widget
-        self._camera_controller_widget = camera_controller_layout
+        self._skelly_cam_widget = skelly_cam_widget
+        self._camera_controller_widget = camera_controller_widget
         self._welcome_to_freemocap_widget = welcome_to_freemocap_widget
-        self._visualize_data_widget = visualize_data_widget
+        self._skelly_viewer_widget = skelly_viewer_widget
 
         self._create_welcome_tab(self)
-        self._create_camera_view_tab(self)
-        self._create_visualization_tab(self)
+        self._create_skellycam_view_tab(self)
+        self._create_skelly_viewer_tab(self)
 
     def set_welcome_tab_enabled(self, enabled: bool):
         self.setTabEnabled(0, enabled)
@@ -38,21 +44,23 @@ class CentralTabWidget(QTabWidget):
     def set_visualize_data_tab_enabled(self, enabled: bool):
         self.setTabEnabled(2, enabled)
 
-    def _create_camera_view_tab(self, tab_widget: QTabWidget):
+    def _create_welcome_tab(self, tab_widget: QTabWidget):
+        logger.info("Creating welcome tab")
+        tab_widget.addTab(self._welcome_to_freemocap_widget, "Welcome!")
+
+    def _create_skellycam_view_tab(self, tab_widget: QTabWidget):
+        logger.info("Creating skellycam view tab")
         dummy_widget = QWidget()
         self._camera_view_layout = QVBoxLayout()
         dummy_widget.setLayout(self._camera_view_layout)
-        tab_widget.addTab(dummy_widget, "Record!")
+        tab_widget.addTab(dummy_widget, "Skelly Cam")
+        # tab_widget.setToolTip(skellycam.__repo_url__)
 
         # self._qt_multi_camera_viewer_widget.resize(1280, 720)
-
         self._camera_view_layout.addWidget(self._camera_controller_widget)
-        self._camera_view_layout.addWidget(self._camera_view_widget)
+        self._camera_view_layout.addWidget(self._skelly_cam_widget)
 
-    def _create_welcome_tab(self, tab_widget: QTabWidget):
-        tab_widget.addTab(self._welcome_to_freemocap_widget, "Welcome!")
-
-    def _create_visualization_tab(self, tab_widget: QTabWidget):
-
-        self._visualize_data_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        tab_widget.addTab(self._visualize_data_widget, "View!")
+    def _create_skelly_viewer_tab(self, tab_widget: QTabWidget):
+        logger.info("Creating visualization tab")
+        tab_widget.addTab(self._skelly_viewer_widget, "Skelly Viewer")
+        # tab_widget.setToolTip(skelly_viewer.__repo_url__)

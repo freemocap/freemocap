59309aafbf49a7cbbcaf35808fad810d5c0540d2
Trent Wirth <trentondwirth@gmail.com>
2023-03-09 15:25:53 -0500

    need to rename the file too...

--- freemocap/core_processes/post_process_skeleton_data/process_single_camera_3d_data.py
+++ freemocap/core_processes/post_process_skeleton_data/process_single_camera_3d_data.py
@@ -4,7 +4,7 @@ import logging
 import numpy as np
 
 from freemocap.core_processes.capture_volume_calibration.triangulate_3d_data import save_mediapipe_3d_data_to_npy
-from freemocap.utilities.flatten_3d_data import project_3d_data_to_z_plane
+from freemocap.utilities.project_3d_data_to_z_plane import project_3d_data_to_z_plane
 
 logger = logging.getLogger(__name__)
 

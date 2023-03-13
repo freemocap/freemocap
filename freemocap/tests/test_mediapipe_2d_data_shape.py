51a340f249c4d182cb1dee086b2ffbaeb7f758ac
Trent Wirth <trentondwirth@gmail.com>
2023-03-13 11:44:24 -0400

    Update test_mediapipe_2d_data_shape.py

--- freemocap/tests/test_mediapipe_2d_data_shape.py
+++ freemocap/tests/test_mediapipe_2d_data_shape.py
@@ -14,9 +14,9 @@ def test_mediapipe_image_data_shape(
 ):
 
     """
-    test that the `mediapipe 2d detection` process worked correctly by checking:
+    test that the `mediapipe image detection` process worked correctly by checking:
 
-    1. There is an `.npy` file containing the mediapipe 2d data in the `output_data_folder`
+    1. There is an `.npy` file containing the mediapipe image data in the `output_data_folder`
     2. The dimensions of that `npy` (number of cameras, number of frames, [ need to do - number of tracked points], [pixelX, pixelY] matches the videos in the `synchronized videos` folder
 
     TODO - check number of tracked points vs 'expected' number of tracked points
@@ -26,11 +26,11 @@ def test_mediapipe_image_data_shape(
         mediapipe_image_data_file_path
     ).is_file(), f"{mediapipe_image_data_file_path} is not a file"
 
-    mediapipe_2d_data = np.load(mediapipe_image_data_file_path)
+    mediapipe_image_data = np.load(mediapipe_image_data_file_path)
 
     list_of_video_paths = list(Path(synchronized_videos_folder).glob("*.mp4"))
     number_of_videos = len(list_of_video_paths)
-    assert mediapipe_2d_data.shape[0] == number_of_videos
+    assert mediapipe_image_data.shape[0] == number_of_videos
 
     frame_count = get_number_of_frames_of_videos_in_a_folder(synchronized_videos_folder)
 
@@ -38,11 +38,11 @@ def test_mediapipe_image_data_shape(
         len(set(frame_count)) == 1
     ), f"Videos in {synchronized_videos_folder} have different frame counts: {frame_count}"
     assert (
-        mediapipe_2d_data.shape[1] == frame_count[0]
+        mediapipe_image_data.shape[1] == frame_count[0]
     ), f"Number of frames in {mediapipe_image_data_file_path} does not match number of frames of videos in {synchronized_videos_folder}"
 
     # TODO - check number of tracked points vs 'expected' number of tracked points
 
-    assert mediapipe_2d_data.shape[3] == 3  # the shape is now 3, x y z TODO - check in with Jon
+    assert mediapipe_image_data.shape[3] == 3  # the shape is now 3, x y z TODO - check in with Jon
 
     return True

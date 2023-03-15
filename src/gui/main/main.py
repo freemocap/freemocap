6bf01574695bfb85814f8b17ca6587bae61d6142
jonmatthis <jonmatthis@gmail.com>
2023-03-15 11:40:16 -0400

    Merge pull request #338
    * re-direct old entry points to `freemocap.__main__.py`

---
+++ src/gui/main/main.py
@@ -0,0 +1,23 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    logger.info(
+        "User tried using `pre-alpha` entry point (`import freemocap: freemocap.RunMe() - displaying friendly message then re-directing to `freemocap.__main__:main()` entry point")
+
+    print("--------------------------------\n"
+          "--------------------------------\n"
+          "--------------------------------\n"
+          "Hello! Looks like you're trying to use the `alpha GUI` entry point for FreeMoCap.\n"
+          "This entry point is deprecated, so we're launching the GUI via `freemocap.__main__:main()` entry point.\n"
+          "If you want use the `alpha GUI` code, use check out the `v0.1.0` tag in the github repo.\n"
+          "Thank you for using FreeMoCap!\n"
+          "--------------------------------\n"
+          "(NOTE  - this entry point will be removed eventually\n"
+          "--------------------------------\n"
+          "--------------------------------\n")
+
+    from freemocap.__main__ import main
+
+    main()

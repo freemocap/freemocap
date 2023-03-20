e299612ebfb885787e6dc8840913dfec71a2c0e2
philipqueen <queen.philip@gmail.com>
2023-03-20 11:20:22 -0600

    basic hacky fix

--- freemocap/gui/qt/widgets/control_panel/visualization_control_panel.py
+++ freemocap/gui/qt/widgets/control_panel/visualization_control_panel.py
@@ -96,6 +96,8 @@ class VisualizationControlPanel(QWidget):
 
         if "blender." in path_selection[0]:
             self._blender_executable_path = path_selection[0]
+        elif "blender." or "Blender" in self._blender_executable_label.text():
+            self._blender_executable_path = self._blender_executable_label.text()
         else:
             self._blender_executable_path = BLENDER_EXECUTABLE_PATH_MISSING_STRING
 

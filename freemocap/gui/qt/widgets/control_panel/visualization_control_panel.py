e78ce6f7ef5f03eff08f137ac268e38313bf9dfa
philipqueen <queen.philip@gmail.com>
2023-03-20 12:13:00 -0600

    Easier to use solution

--- freemocap/gui/qt/widgets/control_panel/visualization_control_panel.py
+++ freemocap/gui/qt/widgets/control_panel/visualization_control_panel.py
@@ -96,8 +96,8 @@ class VisualizationControlPanel(QWidget):
 
         if "blender." in path_selection[0]:
             self._blender_executable_path = path_selection[0]
-        elif "blender." in self._blender_executable_label.text() or "MacOS/Blender" in self._blender_executable_label.text():
-            self._blender_executable_path = self._blender_executable_label.text()
+        elif "Blender.app" in path_selection[0]:
+            self._blender_executable_path = path_selection[0] + "/Contents/MacOS/Blender" # executable is buried on mac
         else:
             self._blender_executable_path = BLENDER_EXECUTABLE_PATH_MISSING_STRING
 

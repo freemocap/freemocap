7d6bd24353fb8ca408208e2ac3f74416fec34d6f
philipqueen <queen.philip@gmail.com>
2023-03-22 15:51:36 -0600

    add get_video_files.py

---
+++ freemocap/utilities/get_video_files.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+def create_list_of_video_paths(path_to_video_folder: Path) -> list:
+    '''Search the folder for 'mp4' files (case insensitive) and return them as a list'''
+
+    list_of_video_paths = list(Path(path_to_video_folder).glob('*.mp4')) + list(Path(path_to_video_folder).glob('*.MP4'))
+    unique_list_of_video_paths = get_unique_list(list_of_video_paths)
+
+    return unique_list_of_video_paths
+
+def get_unique_list(list: list) -> list:
+    '''Return a list of the unique elements from input list'''
+    unique_list = []
+    [unique_list.append(clip) for clip in list if clip not in unique_list]
+
+    return unique_list

1bc2a2c635b5fdbf9d6ff2141eb299e7553d2383
trentwirth <62706609+trentwirth@users.noreply.github.com>
2023-03-24 12:20:09 -0400

    Trent/single camera 2 (#342)
    * created utility functions
    
    - project_3d_data_to_z_plane.py
    - rotate_by_90_degrees_around_x_axis.py
    
    * update `mediapipe_skeleton_detector.py`
    
    * Create process_single_camera_skeleton_data.py
    
    * added single camera spoofing
    
    * added in rotation step
    
    * bug fixes
    
    kwarg names and test assert
    
    * Update freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
    
    Co-authored-by: jonmatthis <jonmatthis@gmail.com>
    
    * fiddly comment clarification
    
    ---------
    
    Co-authored-by: jonmatthis <jonmatthis@gmail.com>

---
+++ freemocap/utilities/project_3d_data_to_z_plane.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def project_3d_data_to_z_plane(skel3d_frame_marker_xyz: np.ndarray) -> np.ndarray:
+
+    projected_skel3d_frame_marker_xyz = np.zeros(skel3d_frame_marker_xyz.shape)
+
+    projected_skel3d_frame_marker_xyz[:, :, 0] = skel3d_frame_marker_xyz[:, :, 0]
+    projected_skel3d_frame_marker_xyz[:, :, 1] = skel3d_frame_marker_xyz[:, :, 1]
+    projected_skel3d_frame_marker_xyz[:, :, 2] = 0
+
+    return projected_skel3d_frame_marker_xyz

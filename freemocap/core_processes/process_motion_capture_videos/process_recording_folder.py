086cb9de9fd64a83b5c518ff7fc72607799768d8
philipqueen <queen.philip@gmail.com>
2023-04-13 09:38:01 -0600

    Mostly workings skips, may change though

--- freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
+++ freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 import multiprocessing
 from pathlib import Path
 
@@ -64,17 +65,25 @@ def process_recording_folder(
             f"Could not find synchronized_videos folder at {rec.recording_info_model.synchronized_videos_folder_path}"
         )
 
+    
     logger.info("Detecting 2d skeletons...")
-    # 2d skeleton detection
-    mediapipe_skeleton_detector = MediaPipeSkeletonDetector(
-        parameter_model=rec.mediapipe_parameters_model,
-    )
+    if rec.mediapipe_parameters_model.skip_2d_image_tracking:
+        try: 
+            mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ = np.load(rec.recording_info_model.mediapipe_2d_data_npy_file_path)
+        except Exception as e:
+            logger.error("Failed to load 2D data, cannot continue processing")
+            return
+    else:
+        # 2d skeleton detection
+        mediapipe_skeleton_detector = MediaPipeSkeletonDetector(
+            parameter_model=rec.mediapipe_parameters_model,
+        )
 
-    mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ = mediapipe_skeleton_detector.process_folder_full_of_videos(
-        rec.recording_info_model.synchronized_videos_folder_path,
-        Path(rec.recording_info_model.output_data_folder_path) / RAW_DATA_FOLDER_NAME,
-        kill_event=kill_event,
-    )
+        mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ = mediapipe_skeleton_detector.process_folder_full_of_videos(
+            rec.recording_info_model.synchronized_videos_folder_path,
+            Path(rec.recording_info_model.output_data_folder_path) / RAW_DATA_FOLDER_NAME,
+            kill_event=kill_event,
+        )
 
     if kill_event is not None and kill_event.is_set():
         return
@@ -92,48 +101,56 @@ def process_recording_folder(
             raw_data_folder_path=Path(rec.recording_info_model.raw_data_folder_path))
 
     else:
+        if rec.anipose_triangulate_3d_parameters_model.skip_3d_triangulation:
+            raw_skel3d_frame_marker_xyz = np.load(rec.recording_info_model.raw_mediapipe_3d_data_npy_file_path)
+            skeleton_reprojection_error_fr_mar = np.load(rec.recording_info_model.mediapipe_reprojection_error_data_npy_file_path)
+        else:
+            logger.info("Triangulating 3d skeletons...")
 
-        logger.info("Triangulating 3d skeletons...")
+            assert rec.recording_info_model.calibration_toml_check, f"No calibration file found at: {rec.recording_info_model.calibration_toml_path}"
+            assert rec.recording_info_model.data2d_status_check, f"No mediapipe 2d data found at: {rec.recording_info_model.mediapipe_2d_data_npy_file_path}"
 
-        assert rec.recording_info_model.calibration_toml_check, f"No calibration file found at: {rec.recording_info_model.calibration_toml_path}"
-        assert rec.recording_info_model.data2d_status_check, f"No mediapipe 2d data found at: {rec.recording_info_model.mediapipe_2d_data_npy_file_path}"
+            anipose_calibration_object = load_anipose_calibration_toml_from_path(
+                camera_calibration_data_toml_path=rec.recording_info_model.calibration_toml_path,
+                save_copy_of_calibration_to_this_path=rec.recording_info_model.path,
+            )
+            (raw_skel3d_frame_marker_xyz, skeleton_reprojection_error_fr_mar,) = triangulate_3d_data(
+                anipose_calibration_object=anipose_calibration_object,
+                mediapipe_2d_data=mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ[:, :, :, :2],
+                output_data_folder_path=rec.recording_info_model.raw_data_folder_path,
+                mediapipe_confidence_cutoff_threshold=rec.anipose_triangulate_3d_parameters_model.confidence_threshold_cutoff,
+                use_triangulate_ransac=rec.anipose_triangulate_3d_parameters_model.use_triangulate_ransac_method,
+                kill_event=kill_event,
+            )
 
-        anipose_calibration_object = load_anipose_calibration_toml_from_path(
-            camera_calibration_data_toml_path=rec.recording_info_model.calibration_toml_path,
-            save_copy_of_calibration_to_this_path=rec.recording_info_model.path,
-        )
-        (raw_skel3d_frame_marker_xyz, skeleton_reprojection_error_fr_mar,) = triangulate_3d_data(
-            anipose_calibration_object=anipose_calibration_object,
-            mediapipe_2d_data=mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ[:, :, :, :2],
-            output_data_folder_path=rec.recording_info_model.raw_data_folder_path,
-            mediapipe_confidence_cutoff_threshold=rec.anipose_triangulate_3d_parameters_model.confidence_threshold_cutoff,
-            use_triangulate_ransac=rec.anipose_triangulate_3d_parameters_model.use_triangulate_ransac_method,
-            kill_event=kill_event,
-        )
+    if kill_event is not None and kill_event.is_set():
+        return
 
-        if kill_event is not None and kill_event.is_set():
-            return
-
-        assert test_mediapipe_3d_data_shape(
-            synchronized_videos_folder=rec.recording_info_model.synchronized_videos_folder_path,
-            mediapipe_3d_data_npy_path=rec.recording_info_model.raw_mediapipe_3d_data_npy_file_path,
-            medipipe_reprojection_error_data_npy_path=rec.recording_info_model.mediapipe_reprojection_error_data_npy_file_path,
-        )
+    assert test_mediapipe_3d_data_shape(
+        synchronized_videos_folder=rec.recording_info_model.synchronized_videos_folder_path,
+        mediapipe_3d_data_npy_path=rec.recording_info_model.raw_mediapipe_3d_data_npy_file_path,
+        medipipe_reprojection_error_data_npy_path=rec.recording_info_model.mediapipe_reprojection_error_data_npy_file_path,
+    )
 
     #rotate so skeleton is closer to 'vertical' in a z-up reference frame
     rotated_raw_skel3d_frame_marker_xyz = rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz) 
 
-    logger.info("Gap-filling, butterworth filtering, origin aligning 3d skeletons, then calculating center of mass ...")
 
-    skel3d_frame_marker_xyz = gap_fill_filter_origin_align_3d_data_and_then_calculate_center_of_mass(
-        raw_skel3d_frame_marker_xyz=rotated_raw_skel3d_frame_marker_xyz,
-        skeleton_reprojection_error_fr_mar=skeleton_reprojection_error_fr_mar,
-        path_to_folder_where_we_will_save_this_data=rec.recording_info_model.output_data_folder_path,
-        sampling_rate=rec.post_processing_parameters_model.framerate,
-        cut_off=rec.post_processing_parameters_model.butterworth_filter_parameters.cutoff_frequency,
-        order=rec.post_processing_parameters_model.butterworth_filter_parameters.order,
-        reference_frame_number=None,
-    )
+    if rec.post_processing_parameters_model.skip_post_processing:
+        skel3d_frame_marker_xyz = rotated_raw_skel3d_frame_marker_xyz
+    else:
+        logger.info("Gap-filling, butterworth filtering, origin aligning 3d skeletons, then calculating center of mass ...")
+
+        skel3d_frame_marker_xyz = gap_fill_filter_origin_align_3d_data_and_then_calculate_center_of_mass(
+            raw_skel3d_frame_marker_xyz=rotated_raw_skel3d_frame_marker_xyz,
+            skeleton_reprojection_error_fr_mar=skeleton_reprojection_error_fr_mar,
+            path_to_folder_where_we_will_save_this_data=rec.recording_info_model.output_data_folder_path,
+            sampling_rate=rec.post_processing_parameters_model.framerate,
+            cut_off=rec.post_processing_parameters_model.butterworth_filter_parameters.cutoff_frequency,
+            order=rec.post_processing_parameters_model.butterworth_filter_parameters.order,
+            reference_frame_number=None,
+        )
+        
     assert test_mediapipe_3d_data_shape(
         synchronized_videos_folder=rec.recording_info_model.synchronized_videos_folder_path,
         mediapipe_3d_data_npy_path=rec.recording_info_model.mediapipe_3d_data_npy_file_path,

b95f3cb329619ac6653bb7283e44e982c6bd5bbc
aaroncherian <aaron.cherian2@gmail.com>
2023-05-08 13:43:01 -0400

    reformatting to function with 'if __name__ = main'

--- ipython_jupyter_notebooks/notebook_generator.py
+++ ipython_jupyter_notebooks/notebook_generator.py
@@ -1,14 +1,17 @@
 import papermill as pm
 from pathlib import Path
 
-path_to_session = r'C:\Users\aaron\FreeMocap_Data\recording_sessions\session_2023-01-17_15_47_19\15_50_24_gmt-5'
 
-path_to_this_directory = Path().resolve()/'notebook_generator'
+def generate_jupyter_notebook(path_to_recording):
+    path_to_this_directory = Path(__file__).parent
 
-res = pm.execute_notebook(
-    path_to_this_directory/'timeseries_plotter.ipynb',
-    path_to_this_directory/'timeseries_plotter_output.ipynb',
-    parameters = dict(path_to_session=path_to_session)
-)
+    res = pm.execute_notebook(
+        path_to_this_directory / 'timeseries_plotter.ipynb',
+        path_to_this_directory / 'timeseries_plotter_output.ipynb',
+        parameters=dict(path_to_session=path_to_recording)
+    )
 
-f = 2
+
+if __name__ == '__main__':
+    path_to_recording= r'D:\2023-05-03_session_aaron_michael_jon\1.0_recordings\recordings_calib_1\sesh_2023-05-03_17_46_22_aaron_michael_jon'
+    generate_jupyter_notebook(path_to_recording)

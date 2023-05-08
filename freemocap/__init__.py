80c88622e8ea5e400521a806e4b94b4de93ac6ad
philipqueen <queen.philip@gmail.com>
2023-05-08 13:40:40 -0600

    copy updated init

--- freemocap/__init__.py
+++ freemocap/__init__.py
@@ -2,14 +2,12 @@
 
 __author__ = """Skelly FreeMoCap"""
 __email__ = "info@freemocap.org"
-__version__ = "v2023.01.1000"
+__version__ = "v1.0.12-rc"
 __description__ = "A free and open source markerless motion capture system for everyone 💀✨"
-
 
 __package_name__ = "freemocap"
 __repo_url__ = f"https://github.com/freemocap/{__package_name__}/"
 __repo_issues_url__ = f"{__repo_url__}issues"
-
 
 import sys
 from pathlib import Path
@@ -25,3 +23,23 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.info(f"Initializing {__package_name__} package, version: {__version__}, from file: {__file__}")
+
+
+def RunMe(*args, **kwargs):
+    logger.info(
+        "User tried using `pre-alpha` entry point (`import freemocap: freemocap.RunMe() - displaying friendly message then re-directing to `freemocap.__main__:main()` entry point")
+
+    print("--------------------------------\n"
+          "--------------------------------\n"
+          "--------------------------------\n"
+          "Hello! Looks like you're trying to use the `pre-alpha` entry point for FreeMoCap.\n"
+          "This entry point is deprecated, so we're launching the GUI via `freemocap.__main__:main()` entry point.\n"
+          "if you need to install the `pre-alpha` code, use `pip install freemocap==0.0.54`"
+          "Thank you for using FreeMoCap!\n"
+          "--------------------------------\n"
+          "--------------------------------\n"
+          "(NOTE  - this entry point will be removed eventually\n"
+          "--------------------------------\n")
+
+    from freemocap.__main__ import main
+    main()

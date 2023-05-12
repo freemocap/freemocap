5808d4d0baefec8454755405cc127f8068c201ab
aaroncherian <aaron.cherian2@gmail.com>
2023-05-12 14:08:41 -0400

    update notebook names

--- ipython_jupyter_notebooks/notebook_generator.py
+++ ipython_jupyter_notebooks/notebook_generator.py
@@ -6,12 +6,12 @@ def generate_jupyter_notebook(path_to_recording:Path):
     path_to_this_directory = Path(__file__).parent
 
     res = pm.execute_notebook(
-        input_path = path_to_this_directory / 'timeseries_plotter.ipynb',
-        output_path = path_to_recording / 'timeseries_plotter_output.ipynb',
+        input_path = path_to_this_directory / 'freemocap_data_visuals_template.ipynb',
+        output_path = path_to_recording / 'freemocap_data_visualizations.ipynb',
         parameters=dict(path_to_recording= str(path_to_recording))
-    )
+    )   
 
 
 if __name__ == '__main__':
-    path_to_recording= Path(r'D:\2023-05-03_session_aaron_michael_jon\1.0_recordings\recordings_calib_1\sesh_2023-05-03_17_46_22_aaron_michael_jon')
+    path_to_recording= Path(r'D:\2023-05-10_session_aaron_michael_jon_milo\1.0_recordings\calibration_one\sesh_2023-05-10_16_39_27_JSM_fun_walk')
     generate_jupyter_notebook(path_to_recording)

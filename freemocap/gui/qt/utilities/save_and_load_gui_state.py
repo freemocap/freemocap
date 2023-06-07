94f53d6ce2b7e211607c7df7bfbd2b522da52467
philipqueen <queen.philip@gmail.com>
2023-06-07 15:56:09 -0600

    create state model and save/load functions

---
+++ freemocap/gui/qt/utilities/save_and_load_gui_state.py
@@ -0,0 +1,24 @@
+import json
+from pydantic import BaseModel
+
+
+class GuiState(BaseModel):
+    send_user_pings: bool = True
+    auto_process_videos_on_save: bool = True
+    generate_jupyter_notebook: bool = True
+    auto_open_in_blender: bool = True
+    # blender_export_method: str
+    # charuco_square_size: int = 39
+
+    # camera_settings:???
+
+
+def save_gui_state(gui_state: GuiState, file_pathstring: str = None):
+    with open(file_pathstring, "w") as file:
+        json.dump(gui_state.dict(), file, indent=4)
+
+
+def load_gui_state(file_pathstring: str = None) -> GuiState:
+    with open(file_pathstring, "r") as file:
+        gui_state = GuiState(**json.load(file))
+    return gui_state

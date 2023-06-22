49bb251bb40bcc3701900eabd77324c507d58b5a
philipqueen <queen.philip@gmail.com>
2023-06-22 13:58:00 -0600

    add test coverage sessino to noxfile

--- noxfile.py
+++ noxfile.py
@@ -5,3 +5,9 @@ def test(session):
     """Run the full test suite on python versions 3.8, 3.9, 3.10, 3.11."""
     session.install("-e", ".")
     session.run("pytest", "freemocap/tests")
+
+@nox.session(python="3.11")
+def coverage(session):
+    """Run a coverage test on python 3.11."""
+    session.install("-e", ".[dev]")
+    session.run("pytest", "--cov=freemocap", "freemocap/tests")

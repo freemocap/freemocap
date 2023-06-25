c9fa9c2a4a5976202882f000fb7a1333084e7c1b
philipqueen <queen.philip@gmail.com>
2023-06-24 22:27:14 -0600

    Add type annotations to noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,14 @@
 import nox
+from nox.sessions import Session
 
 @nox.session(python=["3.8","3.9", "3.10", "3.11"])
-def test(session):
+def test(session: Session):
     """Run the full test suite on python versions 3.8, 3.9, 3.10, 3.11."""
     session.install("-e", ".")
     session.run("pytest", "freemocap/tests")
 
 @nox.session(python="3.11")
-def coverage(session):
+def coverage(session: Session):
     """Run a coverage test on python 3.11."""
     session.install("-e", ".[dev]")
     session.run("pytest", "--cov=freemocap", "freemocap/tests")

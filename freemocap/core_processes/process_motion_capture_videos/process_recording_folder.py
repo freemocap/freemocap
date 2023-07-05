b622bf770ac60ce3eb428a4702215efd372e52e2
jonmatthis <jonmatthis@gmail.com>
2023-07-05 10:17:55 -0400

    fixing conflicts

--- freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
+++ freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
@@ -1,25 +1,9 @@
 import logging
-import numpy as np
 import multiprocessing
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
-
-from freemocap.core_processes.detecting_things_in_2d_images.mediapipe_stuff.mediapipe_skeleton_names_and_connections import (
-    mediapipe_names_and_connections_dict,
-)
-from freemocap.core_processes.post_process_skeleton_data.calculate_center_of_mass import run_center_of_mass_calculations
-from freemocap.core_processes.post_process_skeleton_data.post_process_skeleton import post_process_data, save_array_to_file
-from freemocap.core_processes.post_process_skeleton_data.process_single_camera_skeleton_data import \
-    process_single_camera_skeleton_data
-from freemocap.system.paths_and_files_names import (
-    MEDIAPIPE_BODY_3D_DATAFRAME_CSV_FILE_NAME,
-    RAW_DATA_FOLDER_NAME,
-    RECORDING_PARAMETER_DICT_JSON_FILE_NAME,
-    CENTER_OF_MASS_FOLDER_NAME,
-    SEGMENT_CENTER_OF_MASS_NPY_FILE_NAME,
-    TOTAL_BODY_CENTER_OF_MASS_NPY_FILE_NAME,
-)
 
 from freemocap.core_processes.capture_volume_calibration.anipose_camera_calibration.get_anipose_calibration_object import (
     load_anipose_calibration_toml_from_path,
@@ -36,17 +20,20 @@ from freemocap.core_processes.detecting_things_in_2d_images.mediapipe_stuff.medi
 from freemocap.core_processes.detecting_things_in_2d_images.mediapipe_stuff.mediapipe_skeleton_names_and_connections import (
     mediapipe_names_and_connections_dict,
 )
+from freemocap.core_processes.post_process_skeleton_data.calculate_center_of_mass import run_center_of_mass_calculations
 from freemocap.core_processes.post_process_skeleton_data.estimate_skeleton_segment_lengths import (
     estimate_skeleton_segment_lengths,
     mediapipe_skeleton_segment_definitions,
 )
-from freemocap.core_processes.post_process_skeleton_data.gap_fill_filter_and_origin_align_skeleton_data import (
-    gap_fill_filter_origin_align_3d_data_and_then_calculate_center_of_mass,
-)
+from freemocap.core_processes.post_process_skeleton_data.post_process_skeleton import post_process_data, \
+    save_array_to_file
 from freemocap.core_processes.post_process_skeleton_data.process_single_camera_skeleton_data import \
     process_single_camera_skeleton_data
-
-
+from freemocap.data_layer.data_saver.data_saver import DataSaver
+from freemocap.data_layer.recording_models.post_processing_parameter_models import PostProcessingParameterModel
+from freemocap.system.paths_and_filenames.file_and_folder_names import RAW_DATA_FOLDER_NAME, \
+    SEGMENT_CENTER_OF_MASS_NPY_FILE_NAME, CENTER_OF_MASS_FOLDER_NAME, TOTAL_BODY_CENTER_OF_MASS_NPY_FILE_NAME, \
+    MEDIAPIPE_BODY_3D_DATAFRAME_CSV_FILE_NAME
 from freemocap.tests.test_image_tracking_data_shape import (
     test_image_tracking_data_shape,
 )
@@ -221,3 +208,8 @@ def process_recording_folder(
         file_name="mediapipe_names_and_connections_dict.json",
         dictionary=mediapipe_names_and_connections_dict,
     )
+
+    #TODO - move this data output method *above* the sloppy stuff above and deprecate the sloppy stuff (gracefully enough not to bork hypothetical users' workflows)
+    DataSaver(recording_folder_path=rec.recording_info_model.path).save_all()
+
+    logger.info(f"Done processing {rec.recording_info_model.path}")

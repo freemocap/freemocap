cad2290e6044d9089bd44ae7aa9bca332dfcb9d2
philipqueen <queen.philip@gmail.com>
2023-07-14 14:48:05 -0500

    Add option to synchronize videos on import (#379)
    * Add space to readme
    
    * remove dummy commit on readme
    
    * rename import videos wizard
    
    * synchronize videos by audio checkbox
    
    * synchronizing, but to wrong place
    
    * working messy synchronization
    
    * working dummy synchronize parameters
    
    * better naming+desrciptions in parameter dialog
    
    * disambiguate synchronize video function
    
    * change message, remove dummy variable addition
    
    * remove debugging print statements
    
    * add skelly_synchronize to pyproject dependencies
    
    * Change synchronize layout in import videos dialog
    
    * Remove unnecessary QWidget import
    
    * synchronize videos directly into session folder
    
    * Separate button message and label with parentheses
    
    * make synchronization run in a thread
    
    * pin aniposelib and toml versions
    
    * Make sure active recording is set after synching
    
    * DRYing _handle_import_videos
    
    * move debug plotting out of thread
    
    * remove mediapip pin to conform to main
    
    * Add word wrap to synchronization message
    
    * remove unnecessary import
    
    ---------
    
    Co-authored-by: jonmatthis <jonmatthis@gmail.com>

--- freemocap/gui/qt/main_window/freemocap_main_window.py
+++ freemocap/gui/qt/main_window/freemocap_main_window.py
@@ -52,10 +52,13 @@ from freemocap.gui.qt.widgets.control_panel.process_mocap_data_panel.process_mot
 )
 from freemocap.gui.qt.widgets.control_panel.visualization_control_panel import VisualizationControlPanel
 from freemocap.gui.qt.widgets.directory_view_widget import DirectoryViewWidget
+from freemocap.gui.qt.widgets.import_videos_wizard import ImportVideosWizard
+from freemocap.gui.qt.widgets.log_view_widget import LogViewWidget
 from freemocap.gui.qt.widgets.home_widget import (
     HomeWidget,
 )
-from freemocap.gui.qt.widgets.import_videos_window import ImportVideosWizard
+
+from freemocap.gui.qt.widgets.import_videos_wizard import ImportVideosWizard
 from freemocap.gui.qt.widgets.log_view_widget import LogViewWidget
 from freemocap.gui.qt.utilities.save_and_load_gui_state import (
     GuiState,
@@ -494,7 +497,7 @@ class MainWindow(QMainWindow):
             return
 
         self._import_videos_window = ImportVideosWizard(parent=self,
-                                                        import_videos_path=import_videos_path,
+                                                        import_videos_path=import_videos_path,                                                 kill_thread_event=self._kill_thread_event,
                                                         )
         self._import_videos_window.folder_to_save_videos_to_selected.connect(self._handle_import_videos)
         text = self._import_videos_window.exec()
@@ -511,8 +514,8 @@ class MainWindow(QMainWindow):
             recording_folder_path=self.download_sample_data_thread_worker.sample_data_path
         )
 
-    @pyqtSlot(list, str)
-    def _handle_import_videos(self, video_paths: List[str], folder_to_save_videos: str):
+    @pyqtSlot(list, str, bool)
+    def _handle_import_videos(self, video_paths: List[str], folder_to_save_videos: str, synchronization_bool: bool):
 
         folder_to_save_videos = Path(folder_to_save_videos)
         folder_to_save_videos.mkdir(parents=True, exist_ok=True)
@@ -520,23 +523,24 @@ class MainWindow(QMainWindow):
         if len(video_paths) == 0:
             logger.error("No videos to import!")
             return
+        
+        if not synchronization_bool:
+            for video_path in tqdm(video_paths,
+                                desc="Importing videos...",
+                                colour=[255, 128, 0],
+                                unit="video",
+                                unit_scale=True,
+                                leave=False
+                                ):
 
-        for video_path in tqdm(video_paths,
-                               desc="Importing videos...",
-                               colour=[255, 128, 0],
-                               unit="video",
-                               unit_scale=True,
-                               leave=False
-                               ):
+                if not Path(video_path).exists():
+                    logger.error(f"{video_path} does not exist!")
+                    return
 
-            if not Path(video_path).exists():
-                logger.error(f"{video_path} does not exist!")
-                return
+                destination_path = folder_to_save_videos / Path(video_path).name
+                logger.info(f"Copying video from {video_path} to {destination_path}")
 
-            destination_path = folder_to_save_videos / Path(video_path).name
-            logger.info(f"Copying video from {video_path} to {destination_path}")
-
-            shutil.copy(video_path, destination_path)
+                shutil.copy(video_path, destination_path)
 
         timestamps_copied = copy_directory_if_contains_timestamps(source_dir=Path(video_paths[0]).parent,
                                                                   destination_dir=folder_to_save_videos)

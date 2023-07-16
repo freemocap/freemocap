1e0c3827c1600e51ecf4ffd6acff3af708e7e644
philipqueen <queen.philip@gmail.com>
2023-07-16 08:50:16 -0500

    make noxfile compliant

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,13 @@
 import nox
 from nox.sessions import Session
 
-@nox.session(python=["3.8","3.9", "3.10", "3.11"])
+
+@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def test(session: Session):
     """Run the full test suite on python versions 3.8, 3.9, 3.10, 3.11."""
     session.install("-e", ".")
     session.run("pytest", "freemocap/tests")
+
 
 @nox.session(python="3.11")
 def coverage(session: Session):

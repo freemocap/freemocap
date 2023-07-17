c783f3ae454a355f6f4d676d81f54eacd08d62b3
newturok <newturok@gmail.com>
2023-07-17 14:49:38 +0300

    Update project_3d_data_to_z_plane.py
    In this version, the script makes a copy of the skel3d_frame_marker_xyz array to avoid modifying the original array.
    
    Then, the script directly set the z-coordinate (third dimension) of all elements in the copied array to 0 using array indexing.

--- freemocap/utilities/geometry/project_3d_data_to_z_plane.py
+++ freemocap/utilities/geometry/project_3d_data_to_z_plane.py
@@ -2,10 +2,7 @@ import numpy as np
 
 
 def project_3d_data_to_z_plane(skel3d_frame_marker_xyz: np.ndarray) -> np.ndarray:
-    projected_skel3d_frame_marker_xyz = np.zeros(skel3d_frame_marker_xyz.shape)
-
-    projected_skel3d_frame_marker_xyz[:, :, 0] = skel3d_frame_marker_xyz[:, :, 0]
-    projected_skel3d_frame_marker_xyz[:, :, 1] = skel3d_frame_marker_xyz[:, :, 1]
+    projected_skel3d_frame_marker_xyz = skel3d_frame_marker_xyz.copy()
     projected_skel3d_frame_marker_xyz[:, :, 2] = 0
 
     return projected_skel3d_frame_marker_xyz

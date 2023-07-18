6c5f78ebb77bc1e8ec3d393da7570f316397dca9
philipqueen <queen.philip@gmail.com>
2023-07-18 16:01:27 -0600

    Revert "add error handling for incorrect array size"

--- freemocap/utilities/geometry/rotate_by_90_degrees_around_x_axis.py
+++ freemocap/utilities/geometry/rotate_by_90_degrees_around_x_axis.py
@@ -2,8 +2,6 @@ import numpy as np
 
 
 def rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz: np.ndarray) -> np.ndarray:
-    if len(raw_skel3d_frame_marker_xyz.shape) != 3:
-        raise ValueError("raw_skel3d_frame_marker_xyz must have shape (N, M, 3)")
     swapped_skel3d_frame_marker_xyz = np.zeros(raw_skel3d_frame_marker_xyz.shape)
 
     swapped_skel3d_frame_marker_xyz[:, :, 0] = raw_skel3d_frame_marker_xyz[:, :, 0]

2e16a987023ac3098868c79419d2a773973d2567
philipqueen <queen.philip@gmail.com>
2023-07-18 15:59:17 -0600

    error handling for rotation around x axis

--- freemocap/utilities/geometry/rotate_by_90_degrees_around_x_axis.py
+++ freemocap/utilities/geometry/rotate_by_90_degrees_around_x_axis.py
@@ -3,8 +3,7 @@ import numpy as np
 
 def rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz: np.ndarray) -> np.ndarray:
     if len(raw_skel3d_frame_marker_xyz.shape) != 3:
-        raise ValueError("Input must be of shape (N, M, 3)")
-    
+        raise ValueError("raw_skel3d_frame_marker_xyz must have shape (N, M, 3)")
     swapped_skel3d_frame_marker_xyz = np.zeros(raw_skel3d_frame_marker_xyz.shape)
 
     swapped_skel3d_frame_marker_xyz[:, :, 0] = raw_skel3d_frame_marker_xyz[:, :, 0]

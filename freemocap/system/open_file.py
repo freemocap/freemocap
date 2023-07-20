af06f8c7623c59bd8a8a50e669f27c52d5c7adda
philipqueen <queen.philip@gmail.com>
2023-07-20 15:17:26 -0600

    handle unneeded security warnings

--- freemocap/system/open_file.py
+++ freemocap/system/open_file.py
@@ -5,7 +5,7 @@ import sys
 
 def open_file(filename):
     if sys.platform == "win32":
-        os.startfile(filename)
+        os.startfile(filename)  # noqa
     else:
         opener = "open" if sys.platform == "darwin" else "xdg-open"
         subprocess.call([opener, filename])

aedbd0041cabb4a1a77bb292530db1bbf562f2c7
philipqueen <queen.philip@gmail.com>
2023-07-27 10:45:38 -0600

    change layout a bit

--- freemocap/gui/qt/widgets/welcome_screen_dialog.py
+++ freemocap/gui/qt/widgets/welcome_screen_dialog.py
@@ -18,7 +18,7 @@ class WelcomeScreenDialog(QDialog):
         self.gui_state = gui_state
         self.kill_thread_event = kill_thread_event
 
-        self.setMinimumSize(500, 500)
+        self.setMinimumSize(600, 300)
 
         self.setWindowTitle("Welcome to Freemocap")
 
@@ -28,7 +28,7 @@ class WelcomeScreenDialog(QDialog):
         single_camera_doc_text = "Starting with a single camera recording is a great way to learn to use FreeMoCap.\nClick the link below for a tutorial on getting 2d data from a single camera, without any calibrating."
         single_camera_docs_label = QLabel(single_camera_doc_text)
         single_camera_docs_label.setWordWrap(True)
-        self._layout.addWidget(single_camera_docs_label)
+        self._layout.addWidget(single_camera_docs_label, 1)
         
         single_camera_recording_doc_link_string = '<a href="https://freemocap.readthedocs.io/en/latest/getting_started/single_camera_recording/" style="color: #333333;">Single camera recording tutorial</a>'
         single_camera_doc_link = QLabel(single_camera_recording_doc_link_string)
@@ -38,7 +38,7 @@ class WelcomeScreenDialog(QDialog):
         multi_camera_doc_text = "To get 3d data from FreeMoCap, you need to record with multiple cameras and calibrate them with a charuco board.\nClick the link below for a tutorial on calibrating multiple cameras."
         multi_camera_docs_label = QLabel(multi_camera_doc_text)
         multi_camera_docs_label.setWordWrap(True)
-        self._layout.addWidget(multi_camera_docs_label)
+        self._layout.addWidget(multi_camera_docs_label, 1)
         
         multi_camera_recording_doc_link_string = '<a href="https://freemocap.readthedocs.io/en/latest/getting_started/multi_camera_calibration/" style="color: #333333;">Multi camera recording tutorial</a>'
         multi_camera_doc_link = QLabel(multi_camera_recording_doc_link_string)

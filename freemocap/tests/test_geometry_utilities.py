8c110a4102be80d892e88d426f0ebd9da98da824
philipqueen <queen.philip@gmail.com>
2023-07-28 10:02:31 -0600

    format tests with black

--- freemocap/tests/test_geometry_utilities.py
+++ freemocap/tests/test_geometry_utilities.py
@@ -9,30 +9,40 @@ def test_rotate_by_90_degrees_around_x_axis():
     # test single point
     raw_skel3d_frame_marker_xyz = np.array([[[1, 2, 3]]])
     expected_result = np.array([[[1, 3, -2]]])
-    assert np.allclose(rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz), expected_result), "Did not correctly rotate the single input point."
+    assert np.allclose(
+        rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz), expected_result
+    ), "Did not correctly rotate the single input point."
 
     # test multiple points
     raw_skel3d_frame_marker_xyz = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]]])
     expected_result = np.array([[[1, 3, -2], [4, 6, -5], [7, 9, -8]]])
-    assert np.allclose(rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz), expected_result), "Did not correctly rotate the multiple input points."
+    assert np.allclose(
+        rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz), expected_result
+    ), "Did not correctly rotate the multiple input points."
 
     # test empty input
     raw_skel3d_frame_marker_xyz = np.array([])
     with pytest.raises(ValueError) as exc_info:
         rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz)
-    assert str(exc_info.value) == "raw_skel3d_frame_marker_xyz must have shape (N, M, 3)", "Did not correctly handle an empty input array."
+    assert (
+        str(exc_info.value) == "raw_skel3d_frame_marker_xyz must have shape (N, M, 3)"
+    ), "Did not correctly handle an empty input array."
 
     # test zero input
     raw_skel3d_frame_marker_xyz = np.array([[[0, 0, 0], [0, 0, 0], [0, 0, 0]]])
     expected_result = np.array([[[0, 0, 0], [0, 0, 0], [0, 0, 0]]])
-    assert np.allclose(rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz), expected_result), "Did not correctly handle a zero input array."
+    assert np.allclose(
+        rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz), expected_result
+    ), "Did not correctly handle a zero input array."
 
 
 def test_project_3d_data_to_z_plane():
     # test simple case
     input_data = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
     expected_output = np.array([[[1, 2, 0], [4, 5, 0]], [[7, 8, 0], [10, 11, 0]]])
-    assert np.array_equal(project_3d_data_to_z_plane(input_data), expected_output), f"Test failed for input_data: {input_data}"
+    assert np.array_equal(
+        project_3d_data_to_z_plane(input_data), expected_output
+    ), f"Test failed for input_data: {input_data}"
 
     # test more complex case
     input_data = np.array(
@@ -59,7 +69,11 @@ def test_project_3d_data_to_z_plane():
             [[6.57, 2.48, 0], [5.08, 1.76, 0]],
         ]
     )
-    assert np.array_equal(project_3d_data_to_z_plane(input_data), expected_output), f"Test failed for input_data: {input_data}"
+    assert np.array_equal(
+        project_3d_data_to_z_plane(input_data), expected_output
+    ), f"Test failed for input_data: {input_data}"
 
     # test input data was not changed
-    assert np.array_equal(input_data, input_data_copy), "Input data was modified by function, function should not change the input data."
+    assert np.array_equal(
+        input_data, input_data_copy
+    ), "Input data was modified by function, function should not change the input data."

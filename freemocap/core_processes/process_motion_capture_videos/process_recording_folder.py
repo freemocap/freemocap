924f532355c20c031c8c9a5afecfa25407216c4c
philipqueen <queen.philip@gmail.com>
2023-08-02 11:06:36 -0600

    add reprojection filtering to recording processing

--- freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
+++ freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
@@ -8,7 +8,9 @@ import pandas as pd
 from freemocap.core_processes.capture_volume_calibration.anipose_camera_calibration.get_anipose_calibration_object import (
     load_anipose_calibration_toml_from_path,
 )
+from freemocap.core_processes.capture_volume_calibration.reprojection_filtering import filter_by_reprojection_error
 from freemocap.core_processes.capture_volume_calibration.triangulate_3d_data import (
+    save_mediapipe_3d_data_to_npy,
     triangulate_3d_data,
 )
 from freemocap.core_processes.detecting_things_in_2d_images.mediapipe_stuff.convert_mediapipe_npy_to_csv import (
@@ -159,6 +161,23 @@ def process_recording_folder(
                 use_triangulate_ransac=rec.anipose_triangulate_3d_parameters_model.use_triangulate_ransac_method,
                 kill_event=kill_event,
             )
+            (
+                reprojection_filtered_skel3d_frame_marker_xyz,
+                reprojection_filtered_skeleton_reprojection_error_fr_mar,
+            ) = filter_by_reprojection_error(
+                reprojection_error_frame_marker=skeleton_reprojection_error_fr_mar,
+                reprojection_error_threshold=rec.anipose_triangulate_3d_parameters_model.confidence_threshold_cutoff,
+                mediapipe_2d_data=mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ[:, :, :, :2],
+                raw_skel3d_frame_marker_xyz=raw_skel3d_frame_marker_xyz,
+                anipose_calibration_object=anipose_calibration_object,
+                output_data_folder_path=rec.recording_info_model.raw_data_folder_path,
+                use_triangulate_ransac=rec.anipose_triangulate_3d_parameters_model.use_triangulate_ransac_method,
+            )
+            save_mediapipe_3d_data_to_npy(
+                data3d_numFrames_numTrackedPoints_XYZ=reprojection_filtered_skel3d_frame_marker_xyz,
+                data3d_numFrames_numTrackedPoints_reprojectionError=reprojection_filtered_skeleton_reprojection_error_fr_mar,
+                path_to_folder_where_data_will_be_saved=rec.recording_info_model.raw_data_folder_path,
+            )
 
     if kill_event is not None and kill_event.is_set():
         return
@@ -174,7 +193,7 @@ def process_recording_folder(
 
     logger.info("Using SkellyForge Post-Processing to clean up data...")
 
-    rotated_raw_skel3d_frame_marker_xyz = rotate_by_90_degrees_around_x_axis(raw_skel3d_frame_marker_xyz)
+    rotated_raw_skel3d_frame_marker_xyz = rotate_by_90_degrees_around_x_axis(reprojection_filtered_skel3d_frame_marker_xyz)
 
     skel3d_frame_marker_xyz = post_process_data(
         recording_processing_parameter_model=recording_processing_parameter_model,

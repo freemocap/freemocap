50084305c7461df9d165c0950fbb69aef07bb3c9
philipqueen <queen.philip@gmail.com>
2023-08-07 14:06:46 -0600

    linting

--- freemocap/core_processes/capture_volume_calibration/save_mediapipe_3d_data_to_npy.py
+++ freemocap/core_processes/capture_volume_calibration/save_mediapipe_3d_data_to_npy.py
@@ -4,30 +4,36 @@ from typing import Union
 
 import numpy as np
 
-from freemocap.system.paths_and_filenames.file_and_folder_names import RAW_MEDIAPIPE_3D_NPY_FILE_NAME, \
-    RAW_MEDIAPIPE_REPROJECTION_ERROR_NPY_FILE_NAME, REPROJECTION_FILTERED_MEDIAPIPE_3D_NPY_FILE_NAME, \
-    REPROJECTION_FILTERED_MEDIAPIPE_REPROJECTION_ERROR_NPY_FILE_NAME
+from freemocap.system.paths_and_filenames.file_and_folder_names import (
+    RAW_MEDIAPIPE_3D_NPY_FILE_NAME,
+    RAW_MEDIAPIPE_REPROJECTION_ERROR_NPY_FILE_NAME,
+    REPROJECTION_FILTERED_MEDIAPIPE_3D_NPY_FILE_NAME,
+    REPROJECTION_FILTERED_MEDIAPIPE_REPROJECTION_ERROR_NPY_FILE_NAME,
+)
 
 logger = logging.getLogger(__name__)
 
+
 def save_mediapipe_3d_data_to_npy(
-        data3d_numFrames_numTrackedPoints_XYZ: np.ndarray,
-        data3d_numFrames_numTrackedPoints_reprojectionError: np.ndarray,
-        path_to_folder_where_data_will_be_saved: Union[str, Path],
-        processing_level: str,
+    data3d_numFrames_numTrackedPoints_XYZ: np.ndarray,
+    data3d_numFrames_numTrackedPoints_reprojectionError: np.ndarray,
+    path_to_folder_where_data_will_be_saved: Union[str, Path],
+    processing_level: str,
 ):
     path_to_folder_where_data_will_be_saved = Path(path_to_folder_where_data_will_be_saved)
     Path(path_to_folder_where_data_will_be_saved).mkdir(parents=True, exist_ok=True)  # save spatial XYZ data
     if processing_level == "raw":
         mediapipe_3dData_save_path = path_to_folder_where_data_will_be_saved / RAW_MEDIAPIPE_3D_NPY_FILE_NAME
         mediapipe_reprojection_error_save_path = (
-                path_to_folder_where_data_will_be_saved / RAW_MEDIAPIPE_REPROJECTION_ERROR_NPY_FILE_NAME
-                    )
+            path_to_folder_where_data_will_be_saved / RAW_MEDIAPIPE_REPROJECTION_ERROR_NPY_FILE_NAME
+        )
     elif processing_level == "reprojection_filtered":
-        mediapipe_3dData_save_path = path_to_folder_where_data_will_be_saved / REPROJECTION_FILTERED_MEDIAPIPE_3D_NPY_FILE_NAME
+        mediapipe_3dData_save_path = (
+            path_to_folder_where_data_will_be_saved / REPROJECTION_FILTERED_MEDIAPIPE_3D_NPY_FILE_NAME
+        )
         mediapipe_reprojection_error_save_path = (
-                    path_to_folder_where_data_will_be_saved / REPROJECTION_FILTERED_MEDIAPIPE_REPROJECTION_ERROR_NPY_FILE_NAME
-                    )
+            path_to_folder_where_data_will_be_saved / REPROJECTION_FILTERED_MEDIAPIPE_REPROJECTION_ERROR_NPY_FILE_NAME
+        )
     else:
         logger.exception(f"processing_level: {processing_level} not recognized")
         raise Exception
--- freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
+++ freemocap/core_processes/process_motion_capture_videos/process_recording_folder.py
@@ -12,8 +12,9 @@ from freemocap.core_processes.capture_volume_calibration.reprojection_filtering 
 from freemocap.core_processes.capture_volume_calibration.triangulate_3d_data import (
     triangulate_3d_data,
 )
-from freemocap.core_processes.capture_volume_calibration.save_mediapipe_3d_data_to_npy import \
-    save_mediapipe_3d_data_to_npy
+from freemocap.core_processes.capture_volume_calibration.save_mediapipe_3d_data_to_npy import (
+    save_mediapipe_3d_data_to_npy,
+)
 from freemocap.core_processes.detecting_things_in_2d_images.mediapipe_stuff.convert_mediapipe_npy_to_csv import (
     convert_mediapipe_npy_to_csv,
 )
@@ -57,10 +58,10 @@ logger = logging.getLogger(__name__)
 
 
 def process_recording_folder(
-        recording_processing_parameter_model: PostProcessingParameterModel,
-        kill_event: multiprocessing.Event = None,
-        queue: multiprocessing.Queue = None,
-        use_tqdm: bool = True,
+    recording_processing_parameter_model: PostProcessingParameterModel,
+    kill_event: multiprocessing.Event = None,
+    queue: multiprocessing.Queue = None,
+    use_tqdm: bool = True,
 ):
     """
 
@@ -209,7 +210,8 @@ def process_recording_folder(
     logger.info("Using SkellyForge Post-Processing to clean up data...")
 
     rotated_raw_skel3d_frame_marker_xyz = rotate_by_90_degrees_around_x_axis(
-        reprojection_filtered_skel3d_frame_marker_xyz)
+        reprojection_filtered_skel3d_frame_marker_xyz
+    )
 
     skel3d_frame_marker_xyz = post_process_data(
         recording_processing_parameter_model=recording_processing_parameter_model,
@@ -234,7 +236,7 @@ def process_recording_folder(
         array_to_save=segment_COM_frame_imgPoint_XYZ,
         skeleton_file_name=SEGMENT_CENTER_OF_MASS_NPY_FILE_NAME,
         path_to_folder_where_we_will_save_this_data=Path(path_to_folder_where_we_will_save_this_data)
-                                                    / CENTER_OF_MASS_FOLDER_NAME,
+        / CENTER_OF_MASS_FOLDER_NAME,
     )
 
     logger.info("Saving total body center of mass data")
@@ -242,7 +244,7 @@ def process_recording_folder(
         array_to_save=totalBodyCOM_frame_XYZ,
         skeleton_file_name=TOTAL_BODY_CENTER_OF_MASS_NPY_FILE_NAME,
         path_to_folder_where_we_will_save_this_data=Path(path_to_folder_where_we_will_save_this_data)
-                                                    / CENTER_OF_MASS_FOLDER_NAME,
+        / CENTER_OF_MASS_FOLDER_NAME,
     )
 
     try:
@@ -262,7 +264,7 @@ def process_recording_folder(
     )
 
     path_to_skeleton_body_csv = (
-            Path(rec.recording_info_model.output_data_folder_path) / MEDIAPIPE_BODY_3D_DATAFRAME_CSV_FILE_NAME
+        Path(rec.recording_info_model.output_data_folder_path) / MEDIAPIPE_BODY_3D_DATAFRAME_CSV_FILE_NAME
     )
     skeleton_dataframe = pd.read_csv(path_to_skeleton_body_csv)
 

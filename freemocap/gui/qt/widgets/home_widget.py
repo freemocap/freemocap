feb1360f90bdf2d796a342df6c57ab942372b9a3
Trent <62706609+trentwirth@users.noreply.github.com>
2023-09-14 06:29:41 -0400

    fix broken privacy policy link (#476)

--- freemocap/gui/qt/widgets/home_widget.py
+++ freemocap/gui/qt/widgets/home_widget.py
@@ -132,7 +132,7 @@ class HomeWidget(QWidget):
         hbox = QHBoxLayout()
         self._layout.addLayout(hbox)
         hbox.addStretch(1)
-        privacy_policy_link_string = '<a href="https://freemocap.readthedocs.io/en/latest/privacy_policy/" style="color: #333333;">privacy policy</a>'
+        privacy_policy_link_string = '<a href="https://freemocap.readthedocs.io/en/latest/community/privacy_policy/" style="color: #333333;">privacy policy</a>'
         privacy_policy_link_label = QLabel(privacy_policy_link_string)
         privacy_policy_link_label.setOpenExternalLinks(True)
         hbox.addWidget(privacy_policy_link_label)

b382a5c72c1e664ba8488d03c1debc3b7a498bdd
philipqueen <queen.philip@gmail.com>
2023-10-09 11:04:29 -0600

    handle requests exceptions

--- freemocap/system/user_data/pipedream_pings.py
+++ freemocap/system/user_data/pipedream_pings.py
@@ -24,4 +24,8 @@ class PipedreamPings:
             f"{self._pings_dict} "
         )
 
-        requests.post(self._pipedream_url, json=self._pings_dict, timeout=(5, 60))
+        try:
+            requests.post(self._pipedream_url, json=self._pings_dict, timeout=(5, 60))
+        except requests.RequestException:
+            logger.error(f"Failed to send anonymous ping to pipedream")
+

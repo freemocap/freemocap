d1ad5823f49346a8812c693d4064b69cf460ae14
aaroncherian <aaron.cherian2@gmail.com>
2023-12-08 17:42:08 -0500

    integrating skellytracker

--- freemocap/core_processes/process_motion_capture_videos/processing_pipeline_functions/image_tracking_pipeline_functions.py
+++ freemocap/core_processes/process_motion_capture_videos/processing_pipeline_functions/image_tracking_pipeline_functions.py
@@ -6,6 +6,11 @@ import numpy as np
 from freemocap.core_processes.detecting_things_in_2d_images.mediapipe_stuff.mediapipe_skeleton_detector import (
     MediaPipeSkeletonDetector,
 )
+
+from freemocap.core_processes.detecting_things_in_2d_images.mediapipe_stuff.process_folder_of_videos import (
+    process_folder_of_videos
+)
+
 from freemocap.data_layer.recording_models.post_processing_parameter_models import (
     MediapipeParametersModel,
     ProcessingParameterModel,
@@ -13,6 +18,8 @@ from freemocap.data_layer.recording_models.post_processing_parameter_models impo
 from freemocap.system.logging.queue_logger import DirectQueueHandler
 from freemocap.system.logging.configure_logging import log_view_logging_format_string
 from freemocap.system.paths_and_filenames.file_and_folder_names import RAW_DATA_FOLDER_NAME
+
+from skelly_tracker.trackers.mediapipe_tracker.mediapipe_holistic_tracker import MediapipeHolisticTracker
 
 logger = logging.getLogger(__name__)
 
@@ -65,18 +72,38 @@ def run_image_tracking(
     kill_event: multiprocessing.Event = None,
     use_tqdm: bool = True,
 ):
-    mediapipe_skeleton_detector = MediaPipeSkeletonDetector(
-        parameter_model=tracking_params,
-        use_tqdm=use_tqdm,
-    )
+#     mediapipe_skeleton_detector = MediaPipeSkeletonDetector(
+#         parameter_model=tracking_params,
+#         use_tqdm=use_tqdm,
+#     )
 
-    mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ = (
-        mediapipe_skeleton_detector.process_folder_full_of_videos(
-            path_to_folder_of_videos_to_process=synchronized_videos_folder_path,
-            output_data_folder_path=output_data_folder_path,
-            kill_event=kill_event,
-            use_multiprocessing=tracking_params.use_multiprocessing,
-        )
+    # mediapipe_tracker = MediapipeHolisticTracker(
+    #     model_complexity=tracking_params.mediapipe_model_complexity,
+    #     min_detection_confidence=tracking_params.min_detection_confidence,
+    #     min_tracking_confidence=tracking_params.min_tracking_confidence,
+    #     static_image_mode=tracking_params.static_image_mode,
+    # )
+
+    tracker_type = 'MediapipeHolisticTracker'
+    # tracker_type = 'YOLOMediapipeComboTracker'
+
+    mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ = process_folder_of_videos(
+        tracker_type = tracker_type,
+        tracking_params=tracking_params,
+        synchronized_video_path=synchronized_videos_folder_path,
+        output_path=output_data_folder_path,
+        annotated_video_path=None,
+        use_multiprocessing=tracking_params.use_multiprocessing,
     )
+        
+    
+    # mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ = (
+    #     mediapipe_skeleton_detector.process_folder_full_of_videos(
+    #         path_to_folder_of_videos_to_process=synchronized_videos_folder_path,
+    #         output_data_folder_path=output_data_folder_path,
+    #         kill_event=kill_event,
+    #         use_multiprocessing=tracking_params.use_multiprocessing,
+    #     )
+    # )
 
     return mediapipe_image_data_numCams_numFrames_numTrackedPts_XYZ

45b4306b93a572666a46dab5d0f586c4d1df617c
jonmatthis <jonmatthis@gmail.com>
2023-12-15 08:29:56 -0500

    Bump version v1.0.32 -> v1.0.33

--- freemocap/__init__.py
+++ freemocap/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Skelly FreeMoCap"""
 __email__ = "info@freemocap.org"
-__version__ = "v1.0.32"
+__version__ = "v1.0.33"
 __description__ = "A free and open source markerless motion capture system for everyone 💀✨"
 
 __package_name__ = "freemocap"

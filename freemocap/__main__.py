3e98f4a23235e5433007d35514c3b835651dd3e8
philipqueen <queen.philip@gmail.com>
2024-01-31 22:54:49 -0700

    functionalize and add better error handling

--- freemocap/__main__.py
+++ freemocap/__main__.py
@@ -1,8 +1,9 @@
 # __main__.py
-import logging
 import sys
 from multiprocessing import freeze_support
 from pathlib import Path
+
+from freemocap.utilities.fix_opencv_conflict import fix_opencv_conflict
 
 try:
     from freemocap.gui.qt.freemocap_main import qt_gui_main
@@ -12,26 +13,17 @@ except Exception:
     sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
     from freemocap.gui.qt.freemocap_main import qt_gui_main
 
-logger = logging.getLogger(__name__)
-
 
 def main():
     # set up so you can change the taskbar icon - https://stackoverflow.com/a/74531530/14662833
     import ctypes
     import freemocap
-    import subprocess
+
     from importlib.metadata import distributions
 
     installed_packages = {dist.metadata["Name"] for dist in distributions()}
-    if "opencv-python" and "opencv-contrib-python" in installed_packages:
-        logger.warning("Conflicting versions of opencv found, uninstalling `opencv-python`")
-        try:
-            subprocess.run(["pip", "uninstall", "-y", "opencv-python", "opencv-contrib-python"])
-            subprocess.run(["pip", "install", "opencv-contrib-python==4.8.*"])
-        except Exception:
-            logger.warning(
-                "Failed to uninstall opencv-python, calibration may not work without manually uninstalling it"
-            )
+    if "opencv-python" in installed_packages and "opencv-contrib-python" in installed_packages:
+        fix_opencv_conflict()
 
     if sys.platform == "win32":
         myappid = f"{freemocap.__package_name__}_{freemocap.__version__}"  # arbitrary string

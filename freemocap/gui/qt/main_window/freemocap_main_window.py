256f8d89ea332b255ff6f41e96e4892595f8319b
philipqueen <queen.philip@gmail.com>
2024-02-12 12:18:00 -0700

    Remove opencv import if conflicting dependencies exist (#539)
    * uninstall opencv-python if conflicting versions exist
    
    * functioning, but probably need better error handling
    
    * functionalize and add better error handling
    
    * make dialog for opencv conflict
    
    * change button styling and text

--- freemocap/gui/qt/main_window/freemocap_main_window.py
+++ freemocap/gui/qt/main_window/freemocap_main_window.py
@@ -63,6 +63,7 @@ from freemocap.gui.qt.widgets.home_widget import (
 )
 from freemocap.gui.qt.widgets.import_videos_wizard import ImportVideosWizard
 from freemocap.gui.qt.widgets.log_view_widget import LogViewWidget
+from freemocap.gui.qt.widgets.opencv_conflict_dialog import OpencvConflictDialog
 from freemocap.gui.qt.widgets.welcome_screen_dialog import WelcomeScreenDialog
 from freemocap.gui.qt.workers.download_sample_data_thread_worker import DownloadDataThreadWorker
 from freemocap.gui.qt.workers.export_to_blender_thread_worker import ExportToBlenderThreadWorker
@@ -462,6 +463,13 @@ class MainWindow(QMainWindow):
         )
 
         self._welcome_screen_dialog.exec()
+
+    def open_opencv_conflict_dialog(self):
+        self._opencv_conflict_dialog = OpencvConflictDialog(
+            gui_state=self._gui_state, kill_thread_event=self._kill_thread_event, parent=self
+        )
+
+        self._opencv_conflict_dialog.exec()
 
     def download_data(self, download_url: str):
         logger.info("Downloading sample data")

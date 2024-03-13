784958bf5968dfb8706a337ad26b6f80b8805b27
philipqueen <queen.philip@gmail.com>
2024-03-13 10:55:13 -0600

    try moving logging setup into main

--- freemocap/__init__.py
+++ freemocap/__init__.py
@@ -8,11 +8,3 @@ __description__ = "A free and open source markerless motion capture system for e
 __package_name__ = "freemocap"
 __repo_url__ = f"https://github.com/freemocap/{__package_name__}/"
 __repo_issues_url__ = f"{__repo_url__}issues"
-
-from freemocap.system.logging.configure_logging import configure_logging, LogLevel
-
-configure_logging(LogLevel.TRACE)
-import logging
-
-logger = logging.getLogger(__name__)
-logger.info(f"Initializing {__package_name__} package, version: {__version__}, from file: {__file__}")
--- freemocap/system/logging/configure_logging.py
+++ freemocap/system/logging/configure_logging.py
@@ -121,8 +121,7 @@ class LoggerBuilder:
                 if handler not in logging.getLogger("").handlers:
                     logging.getLogger("").handlers.append(handler)
         else:
-            from freemocap import logger
-
+            logger = logging.getLogger(__name__)
             logger.info("Logging already configured")
 
 

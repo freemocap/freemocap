def51168e5a0acdc32f297cb2f5ada65f96e6241
jonmatthis <jonmatthis@gmail.com>
2024-04-13 12:01:21 -0400

    copying from the `skellycam/jon/tauri-nuxt-ui` branch

--- experimental/freemocap-vue/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import SideBar from '@/components/sidebar/SideBar.vue'
-</script>
-
-<template>
-  <div class="flex bg-secondary w-screen h-screen">
-    <SideBar />
-  </div>
-</template>

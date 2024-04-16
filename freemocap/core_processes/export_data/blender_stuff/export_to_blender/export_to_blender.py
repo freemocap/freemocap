b0bd686b889db86e1f88fbce1bb96d0cb828090d
Aaron Cherian <aaron.cherian2@gmail.com>
2024-04-16 15:35:15 -0400

    Aaron/remove legacy blender code (#572)
    * remove legacy blender from UI
    
    * remove legacy blender options from export to blender
    
    * get rid of legacy blender files
    
    * remove blender export option groupbox
    
    ---------
    
    Co-authored-by: philipqueen <queen.philip@gmail.com>

--- freemocap/core_processes/export_data/blender_stuff/export_to_blender/export_to_blender.py
+++ freemocap/core_processes/export_data/blender_stuff/export_to_blender/export_to_blender.py
@@ -5,15 +5,6 @@ from typing import Union
 from freemocap.core_processes.export_data.blender_stuff.export_to_blender.methods.ajc_addon.run_ajc_addon_main import (
     run_ajc_blender_addon_subprocess,
 )
-from freemocap.core_processes.export_data.blender_stuff.export_to_blender.methods.legacy.run_alpha_megascript import (
-    run_alpha_megascript,
-)
-from freemocap.core_processes.export_data.blender_stuff.export_to_blender.methods.legacy.run_cgtinker_method import (
-    run_cgtinker_method,
-)
-from freemocap.core_processes.export_data.blender_stuff.export_to_blender.methods.legacy.run_megascript_take2 import (
-    run_blender_megascript_take2,
-)
 from freemocap.core_processes.export_data.blender_stuff.get_best_guess_of_blender_path import (
     get_best_guess_of_blender_path,
 )
@@ -33,25 +24,6 @@ def export_to_blender(
 
     if method == "ajc27_blender_addon":
         run_ajc_blender_addon_subprocess(
-            recording_folder_path=recording_folder_path,
-            blender_file_path=blender_file_path,
-            blender_exe_path=blender_exe_path,
-        )
-
-    elif method == "megascript_take2":
-        run_blender_megascript_take2(
-            recording_folder_path=recording_folder_path,
-            blender_file_path=blender_file_path,
-            blender_exe_path=blender_exe_path,
-        )
-    elif method == "alpha_megascript":
-        run_alpha_megascript(
-            recording_folder_path=recording_folder_path,
-            blender_file_path=blender_file_path,
-            blender_exe_path=blender_exe_path,
-        )
-    elif method == "cgtinker":
-        run_cgtinker_method(
             recording_folder_path=recording_folder_path,
             blender_file_path=blender_file_path,
             blender_exe_path=blender_exe_path,

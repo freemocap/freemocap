b48687f5646783096e39cc4aae11400485bbd1e5
jonmatthis <jonmatthis@gmail.com>
2024-04-21 09:34:24 -0400

    nuke everything but the 3d view

--- experimental/tauri-nuxt-app/src-nuxt/components/Camera/CameraGroupView.vue
+++ experimental/tauri-nuxt-app/src-nuxt/components/Camera/CameraGroupView.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="grid grid-cols-[repeat(auto-fit,_minmax(400px,_1fr))] gap-1 p-1 h-full w-screen ">
-    <CameraView
-        v-for="cameraDevice in camerasStore.cameraDevices"
-        :key="cameraDevice.name"
-        :cameraDevice="cameraDevice"
-        class="w-full h-full object-contain  hover:bg-blue-600 hover:border-blue-800 hover:rounded-3xl transition-all ease-out duration-1000"
-    />
-  </div>
+  <v-container fluid class="pa-1">
+    <v-row>
+      <v-col
+          v-for="cameraDevice in camerasStore.cameraDevices"
+          :key="cameraDevice.name"
+      >
+        <CameraView
+            :cameraDevice="cameraDevice"
+            class="w-full h-full transition-swing"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
 const camerasStore = useCamerasStore();
 </script>
+
+<style>
+/* Example of a simple hover effect */
+.transition-swing:hover {
+  transform: scale(1.05);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+}
+</style>

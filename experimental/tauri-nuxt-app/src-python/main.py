bb995631e94156fff033bfd28b72b3d9fa3fac93
jonmatthis <jonmatthis@gmail.com>
2024-04-21 10:42:53 -0400

    fastapi server

--- experimental/tauri-nuxt-app/src-python/main.py
+++ experimental/tauri-nuxt-app/src-python/main.py
@@ -1,1 +1,19 @@
-print("ehlllloooooo!!!!!")
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)

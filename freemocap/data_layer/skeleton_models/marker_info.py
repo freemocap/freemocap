4b82be1c6a74d6b226453ed6a0248a3956ddd2ab
aaroncherian <aaron.cherian2@gmail.com>
2024-05-16 13:57:37 -0400

    update marker info
     - virtual marker validation needs to be in a for loop to work correctly
    
    - pydantic didn't like an underscore before the variable name (i.e. `_all_markers`)

--- freemocap/data_layer/skeleton_models/marker_info.py
+++ freemocap/data_layer/skeleton_models/marker_info.py
@@ -6,29 +6,29 @@ class VirtualMarkerInfo(BaseModel):
     virtual_markers: Dict[str, Dict[str, List[Union[float, str]]]]
 
     @field_validator("virtual_markers")
-    @classmethod
     def validate_virtual_marker(cls, virtual_marker: Dict[str, List[Union[float, str]]]):
-        marker_names = virtual_marker.get("marker_names", [])
-        marker_weights = virtual_marker.get("marker_weights", [])
+        for virtual_marker_name, virtual_marker_values in virtual_marker.items():
+            marker_names = virtual_marker_values.get("marker_names", [])
+            marker_weights = virtual_marker_values.get("marker_weights", [])
 
-        if len(marker_names) != len(marker_weights):
-            raise ValueError(
-                f"The number of marker names must match the number of marker weights for {virtual_marker}. Currently there are {len(marker_names)} names and {len(marker_weights)} weights."
-            )
+            if len(marker_names) != len(marker_weights):
+                raise ValueError(
+                    f"The number of marker names must match the number of marker weights for {virtual_marker_name}. Currently there are {len(marker_names)} names and {len(marker_weights)} weights."
+                )
 
-        if not isinstance(marker_names, list) or not all(isinstance(name, str) for name in marker_names):
-            raise ValueError(f"Marker names must be a list of strings for {marker_names}.")
+            if not isinstance(marker_names, list) or not all(isinstance(name, str) for name in marker_names):
+                raise ValueError(f"Marker names must be a list of strings for {marker_names}.")
 
-        if not isinstance(marker_weights, list) or not all(
-            isinstance(weight, (int, float)) for weight in marker_weights
-        ):
-            raise ValueError(f"Marker weights must be a list of numbers for {virtual_marker}.")
+            if not isinstance(marker_weights, list) or not all(
+                isinstance(weight, (int, float)) for weight in marker_weights
+            ):
+                raise ValueError(f"Marker weights must be a list of numbers for {virtual_marker_name}.")
 
-        weight_sum = sum(marker_weights)
-        if not 0.99 <= weight_sum <= 1.01:  # Allowing a tiny bit of floating-point leniency
-            raise ValueError(
-                f"Marker weights must sum to approximately 1 for {virtual_marker} Current sum is {weight_sum}."
-            )
+            weight_sum = sum(marker_weights)
+            if not 0.99 <= weight_sum <= 1.01:  # Allowing a tiny bit of floating-point leniency
+                raise ValueError(
+                    f"Marker weights must sum to approximately 1 for {virtual_marker_name} Current sum is {weight_sum}."
+                )
 
         return virtual_marker
 
@@ -36,7 +36,7 @@ class VirtualMarkerInfo(BaseModel):
 class MarkerInfo(BaseModel):
     original_marker_names: List[str]
     virtual_marker_definition: Optional[VirtualMarkerInfo] = None
-    _all_markers: List[str] = Field(default_factory=list)
+    all_markers: List[str] = Field(default_factory=list)
 
     @model_validator(mode="before")
     def copy_markers_to_all(cls, values):
@@ -44,20 +44,20 @@ class MarkerInfo(BaseModel):
         original_marker_names = values.get("original_marker_names")
         if original_marker_names:
             # Directly initializing _all_markers with a copy of original_marker_names
-            values["_all_markers"] = original_marker_names.copy()
+            values["all_markers"] = original_marker_names.copy()
         return values
 
     def add_virtual_markers(self, virtual_markers_dict: Dict[str, Dict[str, List[Union[float, str]]]]):
         """Add virtual markers and update _all_markers."""
         self.virtual_marker_definition = VirtualMarkerInfo(virtual_markers=virtual_markers_dict)
         for virtual_marker_name in self.virtual_marker_definition.virtual_markers.keys():
-            if virtual_marker_name not in self._all_markers:
-                self._all_markers.append(virtual_marker_name)
+            if virtual_marker_name not in self.all_markers:
+                self.all_markers.append(virtual_marker_name)
 
     @property
     def all_markers(self) -> List[str]:
         """Publicly expose the combined list of markers."""
-        return self._all_markers
+        return self.all_markers
 
     @classmethod
     def create(cls, marker_list: List[str]):

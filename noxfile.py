668a333e5206284395d439fc9f4201b05b7eebf3
philipqueen <queen.philip@gmail.com>
2024-07-25 23:35:05 -0600

    Support 3.12 (#609)
    * increase version to less than 3.13
    
    * update tests to run 3.12
    
    * update addon version
    
    * fix ajc addon version
    
    * remove skellytracker all spec for testing
    
    * update skelly synchronize and tracker versions
    
    * update versions for 3.12 compatibility - STILL WAITING ON SKELLYFORGE
    
    * bump skellyforge version
    
    * add 3.12 to python versions for nox testing
    
    * split version testing into different test runs

--- noxfile.py
+++ noxfile.py
@@ -2,21 +2,35 @@ import nox
 from nox.sessions import Session
 
 
-@nox.session(python=["3.9", "3.10", "3.11"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def test(session: Session):
-    """Run the full test suite on python versions 3.9, 3.10, 3.11."""
+    """Run the full test suite on python versions 3.9, 3.10, 3.11, and 3.12."""
     session.install("-e", ".")
     session.run("pytest", "freemocap/tests")
 
 
-@nox.session(python="3.11")
+@nox.session(python=["3.9", "3.10"])
+def test_9_10(session: Session):
+    """Run the full test suite on python versions 3.9 and 3.10."""
+    session.install("-e", ".")
+    session.run("pytest", "freemocap/tests")
+
+
+@nox.session(python=["3.11", "3.12"])
+def test_11_12(session: Session):
+    """Run the full test suite on python versions 3.11 and 3.12."""
+    session.install("-e", ".")
+    session.run("pytest", "freemocap/tests")
+
+
+@nox.session(python="3.12")
 def coverage(session: Session):
-    """Run a coverage test on python 3.11."""
+    """Run a coverage test on python 3.12."""
     session.install("-e", ".", "pytest-cov")
     session.run("pytest", "--cov=freemocap", "freemocap/tests")
 
 
-@nox.session(python="3.11")
+@nox.session(python="3.12")
 def lint(session: Session):
     """Lint using Flake8"""
     session.install(

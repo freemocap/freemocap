bdcc83fc3bf3053bd39d8b9d7532ffd6e7e6bc5b
jonmatthis <jonmatthis@gmail.com>
2024-08-30 08:17:18 -0400

    fiddle with tauri/nuxt/vue ui

---
+++ experimental/tauri-nuxt-app/src-nuxt/pages/camera_status.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Camera Status</h1>
+    <p>Cameras Ready: {{ camerasReady }}</p>
+
+    <h2>Available Cameras:</h2>
+    <ul>
+      <li v-for="(camera, index) in camerasStore.cameraDevices" :key="index">
+        {{ camera.deviceInfo.label }} - {{ camera.cameraNumber }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const camerasStore = useCamerasStore();
+const camerasReady = ref(false);
+const cameraDevices = ref([]);
+
+onMounted(async () => {
+  await camerasStore.initialize();
+  camerasReady.value = camerasStore.camerasReady;
+});
+</script>
+
+
+
+
+
+
+
+
+
--- experimental/tauri-nuxt-app/src-nuxt/pages/index.vue
+++ experimental/tauri-nuxt-app/src-nuxt/pages/index.vue
@@ -1,35 +1,63 @@
 <template>
-  <div>
-    <h1>Camera Status</h1>
-    <p>Cameras Ready: {{ camerasReady }}</p>
-
-    <h2>Available Cameras:</h2>
-    <ul>
-      <li v-for="(camera, index) in camerasStore.cameraDevices" :key="index">
-        {{ camera.deviceInfo.label }} - {{ camera.cameraNumber }}
-      </li>
-    </ul>
-  </div>
+  <TresCanvas v-bind="gl">
+    <OrbitControls/>
+    <TresPerspectiveCamera :position="[0, 1, 5]" :near = .01 />
+    <!--    <TresMesh :rotation="[0, 0, 0]" :scale="[1.0, 1.0, 1.0]">-->
+    <TresMesh v-for="(texture, index) in videoTextures" :key="index" :position="cameraPositions[index]" :rotation="[0,0,0]">
+      <TresPlaneGeometry :args="[1.6, .9]"/>
+      <TresMeshBasicMaterial :map="texture"/>
+    </TresMesh>
+    <TresAxesHelper :size="1"/>
+    <TresGridHelper :divisions="100" :size="100" :position="[0,-.001,0]"/>
+  </TresCanvas>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-
+import * as THREE from 'three';
 const camerasStore = useCamerasStore();
-const camerasReady = ref(false);
-const cameraDevices = ref([]);
+const camerasReady = computed(() => camerasStore.camerasReady);
+const videoTextures = ref<THREE.Texture[]>([]);
+
+
+const planeWidth:number = 1.6;
+const planeHeight:number = .9;
+const planeSpacing:number = 0.1;
+
+// Define the type for the array of positions
+type Vector3 = [number, number, number];
+type CameraPositions = ComputedRef<Vector3[]>;
+type CameraPlane  = ComputedRef<Vector3[]>;
+
+const cameraPositions: CameraPositions = computed(() => {
+  return videoTextures.value.map((_:any, index:number, array:THREE.Texture[]): Vector3 => {
+    const offset: number = (planeWidth + planeSpacing) * (array.length - 1) / 2;
+    return [(index * (planeWidth + planeSpacing)) - offset, planeHeight, 0];
+  });
+});
+
+const gl = reactive({
+  clearColor: '#0c352c',
+  antialias: true,
+});
+
 
 onMounted(async () => {
-  await camerasStore.initialize();
-  camerasReady.value = camerasStore.camerasReady;
+  while (!camerasReady.value) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+  for (let i = 0; i < camerasStore.cameraDevices.length; i++) {
+    const video = document.createElement('video');
+    video.autoplay = true;
+    video.style.display = 'none';
+    document.body.appendChild(video);
+
+    video.srcObject = await camerasStore.cameraDevices[i].getStream();
+    video.play();
+
+    const texture = new THREE.VideoTexture(video);
+    videoTextures.value.push(texture);
+  }
+
 });
 </script>
-
-
-
-
-
-
-
-
 

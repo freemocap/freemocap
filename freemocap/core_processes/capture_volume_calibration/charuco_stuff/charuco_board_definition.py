bcc36271879410a43cc4aa287da55df377c3b378
philipqueen <queen.philip@gmail.com>
2024-09-03 15:53:47 -0700

    change charuco definition

--- freemocap/core_processes/capture_volume_calibration/charuco_stuff/charuco_board_definition.py
+++ freemocap/core_processes/capture_volume_calibration/charuco_stuff/charuco_board_definition.py
@@ -6,16 +6,16 @@ import cv2
 
 @dataclass
 class CharucoBoardDefinition:
-    aruco_marker_dict: Dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+    aruco_marker_dict: cv2.aruco.Dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 
-    number_of_squares_width: int = 7
-    number_of_squares_height: int = 5
+    number_of_squares_width: int = 5
+    number_of_squares_height: int = 3
     black_square_side_length: int = 1
     aruco_marker_length_proportional: float = 0.8
 
     def __post_init__(self):
         self.charuco_board = cv2.aruco.CharucoBoard(
-            size=[self.number_of_squares_width, self.number_of_squares_height],
+            size=(self.number_of_squares_width, self.number_of_squares_height),
             squareLength=self.black_square_side_length,
             markerLength=self.aruco_marker_length_proportional,
             dictionary=self.aruco_marker_dict,

a0b4f78f4735694cb1b136f4397bf0aeabe20d24
philipqueen <queen.philip@gmail.com>
2024-09-11 12:16:20 -0700

    more explicit error messages within the pipeline check

--- freemocap/core_processes/process_motion_capture_videos/processing_pipeline_functions/pipeline_check.py
+++ freemocap/core_processes/process_motion_capture_videos/processing_pipeline_functions/pipeline_check.py
@@ -10,24 +10,30 @@ def processing_pipeline_check(processing_parameters: ProcessingParameterModel) -
     status_check_dict = processing_parameters.recording_info_model.status_check
     if not status_check_dict["synchronized_videos_status_check"]:
         raise FileNotFoundError(
-            f"Could not find synchronized_videos folder at {processing_parameters.recording_info_model.synchronized_videos_folder_path}"
+            f"Could not find synchronized_videos folder at {processing_parameters.recording_info_model.synchronized_videos_folder_path}\n"
+            "Synchronized videos are required to run the processing pipeline"
         )
 
     if not processing_parameters.tracking_parameters_model.run_image_tracking:
         if not status_check_dict["data2d_status_check"]:
             raise FileNotFoundError(
-                f"No 2d data found at: {processing_parameters.recording_info_model.data_2d_npy_file_path}"
+                f"No 2d data found at: {processing_parameters.recording_info_model.data_2d_npy_file_path}\n"
+                "2d data is required if you have turned off `Run 2dimage tracking?` in the processing parameters"
             )
 
     if not processing_parameters.anipose_triangulate_3d_parameters_model.run_3d_triangulation:
         if not status_check_dict["data3d_status_check"] and not status_check_dict["single_video_check"]:
             raise FileNotFoundError(
-                f"No 3d data found at: {processing_parameters.recording_info_model.data_3d_npy_file_path}"
+                f"No 3d data found at: {processing_parameters.recording_info_model.data_3d_npy_file_path}\n"
+                "3d data is required if you have selected `Run 3d triangulation?` in the processing parameters"
+                " and have more than 1 video in your synchronized videos folder"
             )
     else:
         if not status_check_dict["calibration_toml_check"] and not status_check_dict["single_video_check"]:
             raise FileNotFoundError(
-                f"No calibration file found at: {processing_parameters.recording_info_model.calibration_toml_path}"
+                f"No calibration file found at: {processing_parameters.recording_info_model.calibration_toml_path}\n"
+                "A calibration.toml file is required if you have selected `Run 3d triangulation?` in the processing parameters"
+                " and have more than 1 video in your synchronized videos folder"
             )
 
 

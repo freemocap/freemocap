04b068218a4fab07652cd25368db932b6e8de1c7
jonmatthis <jonmatthis@gmail.com>
2024-12-21 21:50:57 -0500

    ok v close ithink

--- freemocap/freemocap_app/freemocap_app_state.py
+++ freemocap/freemocap_app/freemocap_app_state.py
@@ -7,6 +7,9 @@ from skellycam.core.camera_group.shmorchestrator.shared_memory.multi_frame_escap
     MultiFrameEscapeSharedMemoryRingBuffer
 from skellycam.skellycam_app.skellycam_app_controller.skellycam_app_controller import create_skellycam_app_controller
 from skellycam.skellycam_app.skellycam_app_state import SkellycamAppState, get_skellycam_app_state
+from skellycam.core.camera_group.shmorchestrator.shared_memory.ring_buffer_camera_shared_memory import \
+    RingBufferCameraSharedMemory
+from skellycam.core import CameraId
 
 from freemocap.pipelines.dummy_pipeline import DummyProcessingServer, DummyPipelineConfig
 
@@ -18,6 +21,7 @@ class FreemocapAppState:
     global_kill_flag: multiprocessing.Value
     pipeline_shutdown_event: multiprocessing.Event
     skellycam_app_state: SkellycamAppState
+    camera_ring_buffer_shms: Optional[dict[CameraId, RingBufferCameraSharedMemory]] = None
 
     @classmethod
     def create(cls, global_kill_flag: multiprocessing.Value):
@@ -39,15 +43,40 @@ class FreemocapAppState:
     def skellycam_ipc_queue(self):
         return self.skellycam_app_state.ipc_queue
 
+
+    def get_camera_ring_buffer_shm_dtos(self) -> dict[CameraId, RingBufferCameraSharedMemory]:
+        if not self.camera_ring_buffer_shms:
+            self._create_camera_ring_buffer_shms()
+        return {camera_id: shm.to_dto() for camera_id, shm in self.camera_ring_buffer_shms.items()}
+
+    def _create_camera_ring_buffer_shms(self):
+        if self.skellycam_app_state.camera_group is None:
+            raise ValueError("Cannot get RingBufferCameraSharedMemory without CameraGroup!")
+        self.camera_ring_buffer_shms =  {camera_id: RingBufferCameraSharedMemory.create(camera_config=config,
+                                                               memory_allocation=100_000_000,  # 100 MB per camera
+                                                               read_only=False)
+                for camera_id, config in self.skellycam_app_state.camera_group.camera_configs.items()}
+
     def create_processing_server(self) -> DummyProcessingServer:
         if not self.frame_escape_shm:
             raise ValueError("Cannot create image processing server without frame escape shared memory!")
         self.pipeline_shutdown_event.clear()
-        return DummyProcessingServer.create(camera_shm_dtos=self.skellycam_app_state.camera_ring_buffer_shms,
+
+
+        return DummyProcessingServer.create(camera_shm_dtos=self.get_camera_ring_buffer_shm_dtos(),
                                             pipeline_config=DummyPipelineConfig.create(
                                                 camera_ids=self.skellycam_app_state.camera_group.camera_ids),
                                             shutdown_event=self.pipeline_shutdown_event,
                                             )
+
+
+    def close(self):
+        self.pipeline_shutdown_event.set()
+        self.skellycam_app_state.close()
+
+        if self.camera_ring_buffer_shms:
+            for camera_id, shm in self.camera_ring_buffer_shms.items():
+                shm.close()
 
 
 FREEMOCAP_APP_STATE: Optional[FreemocapAppState] = None

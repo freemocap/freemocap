ef0d0698a4470a7960d0e3ad0c1015d6eb6c75dd
Aaron Cherian <aaron.cherian2@gmail.com>
2025-03-12 14:01:52 -0400

    fix path bug

--- freemocap/diagnostics/calculate_diagnostics.py
+++ freemocap/diagnostics/calculate_diagnostics.py
@@ -98,7 +98,7 @@ def run(path_to_recording:Path,
 if __name__ == "__main__":
 
     model_info = MediapipeModelInfo()
-    path_to_recording = Path(get_sample_session_path())
+    path_to_recording = Path("/home/runner/freemocap_data/recording_sessions/freemocap_test_data") #hardcoded for the github action location
     freemocap_version = 'current'
     run(path_to_recording=path_to_recording,
         model_info=model_info,

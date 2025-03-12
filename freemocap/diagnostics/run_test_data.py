7229866beb35ea4a2ab821f6b0f09a6f4514ac4e
Aaron Cherian <aaron.cherian2@gmail.com>
2025-03-12 19:12:23 -0400

    oops

--- freemocap/diagnostics/run_test_data.py
+++ freemocap/diagnostics/run_test_data.py
@@ -25,7 +25,7 @@ def setup_session():
     """
     logger.info("Downloading sample data...")
     # SessionInfo.sample_session_folder_path = Path(r'/home/runner/work/freemocap_fork/freemocap_fork/freemocap/freemocap_test_data')
-    SessionInfo.sample_session_folder_path = download_sample_data(sample_data_zip_file_url="https://github.com/aaroncherian/freemocap_fork/releases/download/v0.0.1-alpha/freemocap_test_data._good_cal.zip")
+    SessionInfo.sample_session_folder_path = download_sample_data(sample_data_zip_file_url='https://github.com/aaroncherian/freemocap_fork/releases/download/v0.0.1-alpha/freemocap_test_data.zip')
 
     logger.info("Finding calibration file...")
     calibration_toml_path = find_calibration_toml_path(SessionInfo.sample_session_folder_path)

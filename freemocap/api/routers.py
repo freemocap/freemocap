2f7a881a944c9fd14b825a0ac5ea8df99205a0d4
jonmatthis <jonmatthis@gmail.com>
2025-03-18 05:08:37 -0400

    rm ui router remnants

--- freemocap/api/routers.py
+++ freemocap/api/routers.py
@@ -3,13 +3,9 @@ from skellycam import SKELLYCAM_ROUTERS
 from freemocap.api.http.app.health import health_router
 from freemocap.api.http.app.shutdown import app_shutdown_router
 from freemocap.api.http.app.state import state_router
-from freemocap.api.http.ui.ui_router import ui_router
 from freemocap.api.websocket.websocket_connect import freemocap_websocket_router
 
 FREEMOCAP_ROUTERS = {
-    "/ui": {
-        "ui": ui_router
-    },
     "/app": {
         "health": health_router,
         "state": state_router,

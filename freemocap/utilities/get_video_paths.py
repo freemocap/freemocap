47651996cf4e78600e7150d56fceb2f1022e556a
Aaron Cherian <aaron.cherian2@gmail.com>
2025-03-19 09:13:38 -0400

    add numeric sorting to paths

--- freemocap/utilities/get_video_paths.py
+++ freemocap/utilities/get_video_paths.py
@@ -1,21 +1,24 @@
 from pathlib import Path
 from typing import Union, List
-
+import re
 
 def get_video_paths(path_to_video_folder: Union[str, Path]) -> List[Path]:
-    """Search the folder for 'mp4' files (case insensitive) and return them as a list"""
+    """Search the folder for 'mp4' files (case insensitive) and return them as a sorted unique list"""
 
     list_of_video_paths = list(Path(path_to_video_folder).glob("*.mp4")) + list(
         Path(path_to_video_folder).glob("*.MP4")
     )
     unique_list_of_video_paths = get_unique_list(list_of_video_paths)
 
-    return unique_list_of_video_paths
+    return sorted(unique_list_of_video_paths, key=numeric_sort_key)
 
-
-def get_unique_list(list: list) -> list:
+def get_unique_list(lst: list) -> list:
     """Return a list of the unique elements from input list"""
     unique_list = []
-    [unique_list.append(element) for element in list if element not in unique_list]
-
+    [unique_list.append(element) for element in lst if element not in unique_list]
     return unique_list
+
+def numeric_sort_key(path: Path):
+    """Extracts numbers from filename for natural sorting"""
+    numbers = re.findall(r'\d+', path.name)
+    return [int(num) for num in numbers] if numbers else [float('inf')]

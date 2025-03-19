a8932a440d48fe361daefe7492ec74ad9bf621dd
Aaron Cherian <aaron.cherian2@gmail.com>
2025-03-18 23:11:07 -0400

    both sorting and commenting out paths

--- freemocap/core_processes/capture_volume_calibration/anipose_camera_calibration/anipose_camera_calibrator.py
+++ freemocap/core_processes/capture_volume_calibration/anipose_camera_calibration/anipose_camera_calibrator.py
@@ -44,7 +44,7 @@ class AniposeCameraCalibrator:
     def _get_video_paths(
         self,
     ):
-        self._list_of_video_paths = get_video_paths(path_to_video_folder=Path(self._calibration_videos_folder_path))
+        self._list_of_video_paths = sorted(get_video_paths(path_to_video_folder=Path(self._calibration_videos_folder_path)))
         
     def _initialize_anipose_objects(self):
         list_of_camera_names = [this_video_path.stem for this_video_path in self._list_of_video_paths]
@@ -80,10 +80,10 @@ class AniposeCameraCalibrator:
         success_str = "Anipose Calibration Successful!"
         logger.info(success_str)
         self._progress_callback(success_str)
-        if pin_camera_0_to_origin:
-            # translate cameras so camera0 is on `0,0,0`
-            self._anipose_camera_group_object = self.pin_camera_zero_to_origin(self._anipose_camera_group_object)
-            # self._anipose_camera_group_object = self.rotate_cameras_so_camera_zero_aligns_with_XYZ(self._anipose_camera_group_object)
+        # if pin_camera_0_to_origin:
+        #     # translate cameras so camera0 is on `0,0,0`
+        #     self._anipose_camera_group_object = self.pin_camera_zero_to_origin(self._anipose_camera_group_object)
+        #     # self._anipose_camera_group_object = self.rotate_cameras_so_camera_zero_aligns_with_XYZ(self._anipose_camera_group_object)
 
         # save calibration info to files
         calibration_toml_filename = create_camera_calibration_file_name(

2cf2e574f2e928fcb9f073d9b542f215987d8d78
Aaron Cherian <aaron.cherian2@gmail.com>
2025-04-17 17:57:39 -0400

    Update anipose_camera_calibrator.py

--- freemocap/core_processes/capture_volume_calibration/anipose_camera_calibration/anipose_camera_calibrator.py
+++ freemocap/core_processes/capture_volume_calibration/anipose_camera_calibration/anipose_camera_calibrator.py
@@ -115,13 +115,22 @@ class AniposeCameraCalibrator:
         return calibration_folder_toml_path
 
     def pin_camera_zero_to_origin(self, _anipose_camera_group_object):
+        import cv2
+
         original_translation_vectors = _anipose_camera_group_object.get_translations()
+        original_rotation_vectors = _anipose_camera_group_object.get_rotations()
         camera_0_translation = original_translation_vectors[0, :]
+        camera_0_rotation = original_rotation_vectors[0,:]
+
+        R0, _ = cv2.Rodrigues(camera_0_rotation)  #gets 3x3 rotation matrix for world -> cam 0
+        delta_to_origin_world = - R0.T @ camera_0_translation #the vector that moves camera 0 to the origin in the world reference frame (https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html)
+        
+
         altered_translation_vectors = np.zeros(original_translation_vectors.shape)
-        for this_camera_number in range(original_translation_vectors.shape[0]):
-            altered_translation_vectors[this_camera_number, :] = (
-                original_translation_vectors[this_camera_number, :] - camera_0_translation
-            )
+        for cam_i in range(original_translation_vectors.shape[0]):
+            Ri, _ = cv2.Rodrigues(original_rotation_vectors[cam_i,:]) #gets 3x3 rotation matrix for world -> cam i
+            delta_to_origin_camera_i = Ri @ delta_to_origin_world #changes the delta origin vector from world reference into cam i reference
+            altered_translation_vectors[cam_i, :] = original_translation_vectors[cam_i,:] + delta_to_origin_camera_i 
 
         _anipose_camera_group_object.set_translations(altered_translation_vectors)
         logger.debug(f"original translation vectors:\n {original_translation_vectors}")

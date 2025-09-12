c15843c19e9255fe621404d8c66ab8326635008a
Pooya Deperson <poamrd@gmail.com>
2025-09-12 23:16:47 +0200

    update on css util mode

--- app-design-v2/src/App.tsx
+++ app-design-v2/src/App.tsx
@@ -3,39 +3,46 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import React from "react";
+import "./App.css";
+
 function App() {
   return (
-    <div className="app-container">
+    <div className="flex flex-col h-full border-2 rounded-lg overflow-hidden">
       {/* Top Red Bar */}
-      <div className="top-bar"></div>
+      <div className="bg-red h-40" />
 
-      {/* Main Content Wrapper */}
-      <div className="main-content">
-        {/* Blue Header Bar */}
-        <div className="header-bar"></div>
+      {/* Main Content */}
+      <div className="flex flex-col flex-1">
+        {/* Blue header */}
+        <div className="bg-blue h-30 m-1 rounded-md" />
 
-        <div className="content-body">
-          {/* Left + Middle grouped */}
-          <div className="main-left">
-            <div className="left-panel"></div>
-            <div className="middle-section">
-              <div className="middle-top"></div>
-              <div className="middle-bottom"></div>
+        {/* Body */}
+        <div className="flex flex-1 p-1 gap-2">
+          {/* Left + Middle group */}
+          <div className="flex gap-2 flex-3">
+            {/* Left panel */}
+            <div className="flex-15 bg-black rounded-md" />
+
+            {/* Middle stacked */}
+            <div className="flex flex-col gap-2 flex-15">
+              <div className="flex-1 bg-black rounded-md" />
+              <div className="flex-1 bg-black rounded-md" />
             </div>
           </div>
 
-          {/* Right Section */}
-          <div className="right-section">
-            <div className="right-top"></div>
-            <div className="right-bottom"></div>
+          {/* Right column */}
+          <div className="flex flex-col gap-2 flex-1">
+            <div className="flex-1 bg-pink rounded-md" />
+            <div className="flex-1 bg-pink rounded-md" />
           </div>
         </div>
       </div>
 
-      {/* Bottom Section */}
-      <div className="bottom-section">
-        <div className="bottom-top-bar"></div>
-        <div className="bottom-inner"></div>
+      {/* Bottom section (with top bar inside) */}
+      <div className="h-100 m-1 border-2 rounded-md flex flex-col">
+        <div className="h-25 border-b-2" />
+        <div className="flex-1 rounded-md" />
       </div>
     </div>
   );

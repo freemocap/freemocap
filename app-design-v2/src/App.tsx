afda454415a825a630e453f800b0955bf41600fb
Pooya Deperson <poamrd@gmail.com>
2025-09-13 16:08:29 +0200

    adjusting layout

--- app-design-v2/src/App.tsx
+++ app-design-v2/src/App.tsx
@@ -6,16 +6,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex flex-col h-full border-2 border-black br-5 overflow-hidden">
+    <div className="app-content bg-black flex flex-col p-1 gap-1 h-full overflow-hidden">
       {/* top-header */}
-      <div className="top-header bg-red h-25" />
+      <div className="top-header br-2 h-25" />
 
       {/* main-container */}
       <div className="main-container overflow-hidden flex flex-row flex-1">
         {/* mode-container */}
-        <div className="mode-container overflow-hidden flex flex-col flex-1 gap-2 m-1">
+        <div className="mode-container border-mid-black border-1 .bg-mid-black overflow-hidden flex flex-col flex-1 gap-1 p-1">
           {/* header-tool-bar */}
-          <div className="header-tool-bar bg-blue h-40 br-2 h-30" />
+          <div className="header-tool-bar h-40 br-2 h-30" />
 
           {/* visualize-container */}
           <div className="visualize-container flex gap-2 flex-3">
@@ -31,7 +31,7 @@ function App() {
         </div>
 
         {/* action container -- right right */}
-        <div className="action-container overflow-y min-w-200 max-w-300 flex flex-col gap-2 flex-1 p-1">
+        <div className="action-container border-mid-black border-1 .bg-mid-black overflow-y min-w-200 max-w-300 flex flex-col gap-1 flex-1 p-1">
           <div className="flex-1 bg-pink br-2" />
           <div className="flex-1 bg-pink br-2" />
         </div>
@@ -40,7 +40,7 @@ function App() {
       {/* bottom info-container */}
       <div className="bottom-info-container h-100 m-1 border-2 border-black br-2 flex flex-col">
         <div className="info-header-control h-25 border-2 border-black br-2" />
-        <div className="info-container flex-1 br-2" />
+        <div className="info-container flex-1 br-2" />  
       </div>
     </div>
   );

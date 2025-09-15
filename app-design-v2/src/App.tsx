a7d41266a918fd8ff5945cf3c75028212524a5c3
Pooya Deperson <poamrd@gmail.com>
2025-09-15 21:17:04 +0200

    adding-modal---icon-setup-css---hover-transitions---removed-conflict-css-in-indexcss

--- app-design-v2/src/App.tsx
+++ app-design-v2/src/App.tsx
@@ -3,44 +3,50 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import React from "react";
 import "./App.css";
+import SplashModal from "./SplashModal"; // imported modal
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true); // modal state
+
   return (
-    <div className="app-content bg-black flex flex-col p-1 gap-1 h-full overflow-hidden">
+    <div className="app-content bg-middark flex flex-col p-1 gap-1 h-full overflow-hidden">
+      {/* splash modal */}
+      {showSplash && <SplashModal onClose={() => setShowSplash(false)} />}
+
       {/* top-header */}
       <div className="top-header br-2 h-25" />
 
       {/* main-container */}
       <div className="main-container gap-1 overflow-hidden flex flex-row flex-1">
         {/* mode-container */}
-        <div className="mode-container br-2 bg-mid-black border-mid-black border-2 .bg-mid-black overflow-hidden flex flex-col flex-1 gap-1 p-1">
+        <div className="mode-container br-2 bg-darkgray border-mid-black border-2 .bg-darkgray overflow-hidden flex flex-col flex-1 gap-1 p-1">
           {/* header-tool-bar */}
           <div className="header-tool-bar h-40 br-2 h-30" />
 
           {/* visualize-container */}
           <div className="visualize-container overflow-hidden flex gap-2 flex-3">
-            {/* 3d-container  */}
-            <div className="3d-container flex-15 bg-black br-2" />
+            {/* 3d-container */}
+            <div className="3d-container flex-15 bg-middark br-2" />
 
             {/* video-container */}
             <div className="video-container overflow-y flex flex-col gap-2 flex-15">
-              <div className="flex-1 bg-black br-2" />
-              <div className="flex-1 bg-black br-2" />
+              <div className="flex-1 bg-middark br-2" />
+              <div className="flex-1 bg-middark br-2" />
             </div>
           </div>
         </div>
 
-        {/* action container -- right right */}
-        <div className="action-container overflow-y bg-mid-black br-2 border-mid-black border-2 .bg-mid-black overflow-y min-w-200 max-w-300 flex flex-col gap-1 flex-1 p-1">
-          <div className="subaction-container calibrate-container flex-1 br-1 p-1 gap-1 bg-black" />
-          <div className="subaction-container record-container flex-1 br-1 p-1 gap-1 bg-black" />
-          <div className="subaction-container properties-container flex-1 br-1 p-1 gap-1 bg-black" />
+        {/* action container */}
+        <div className="action-container overflow-y bg-darkgray br-2 border-mid-black border-2 .bg-darkgray overflow-y min-w-200 max-w-300 flex flex-col gap-1 flex-1 p-1">
+          <div className="subaction-container calibrate-container flex-1 br-1 p-1 gap-1 bg-middark" />
+          <div className="subaction-container record-container flex-1 br-1 p-1 gap-1 bg-middark" />
+          <div className="subaction-container properties-container flex-1 br-1 p-1 gap-1 bg-middark" />
         </div>
       </div>
 
       {/* bottom info-container */}
-      <div className="bottom-info-container bg-black border-mid-black h-100 p-1 border-2 border-black br-2 flex flex-col">
-        <div className="info-header-control h-25 bg-black" />
+      <div className="bottom-info-container bg-middark border-mid-black h-100 p-1 border-2 border-black br-2 flex flex-col">
+        <div className="info-header-control h-25 bg-middark" />
         <div className="info-container flex flex-col flex-1 br-2 p-1 gap-1" />  
       </div>
     </div>

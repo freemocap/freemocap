9b75748be7c8c98bcd107def0df44b7f0a315817
Pooya Deperson <poamrd@gmail.com>
2025-09-16 10:20:17 +0200

    splash-screen-style-and-images

--- app-design-v2/src/components/SplashModal.tsx
+++ app-design-v2/src/components/SplashModal.tsx
@@ -15,42 +15,44 @@ const SplashModal: React.FC<SplashModalProps> = ({ onClose }) => {
         className="splash-modal main-container br-2 flex flex-col p-1 bg-dark border-1 border-black"
         onClick={(e) => e.stopPropagation()} // prevent overlay click from closing when clicking inside
       >
-        <div className="flex-1 bg-middark splash-modal-inner-container br-1 gap-3 flex flex-col p-2">
-        {/* close button top-right */}
-       
-          <button onClick={onClose} className="button icon-button close-button pos-abs top-0 right-0 m-1">
+        <div className="visualize-container overflow-hidden flex-1 bg-middark splash-modal-inner-container br-1 gap-3 flex flex-row p-2">
+          {/* close button top-right */}
+
+          <button
+            onClick={onClose}
+            className="button icon-button close-button pos-abs top-0 right-0 m-1"
+          >
             <span className="icon close-icon icon-size-16"></span>
           </button>
-       
-        {/* inner content wrapper */}
-        <div className="visualize-container flex flex-1 gap-2 overflow-hidden">
+
+          {/* inner content wrapper */}
+          {/* <div className="visualize-container flex flex-1 gap-2 overflow-hidden"> */}
           {/* left column */}
-          <div className="3d-container flex-1 br-2 flex flex-col items-center justify-center gap-2 splash-left-col">
-            <div className="splash-image flex items-center justify-center">
-              IMG
-            </div>
-            <div className="splash-icon flex items-center justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="white"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <circle cx="12" cy="12" r="10" />
-              </svg>
-            </div>
+          {/* <div className="image-container flex-1 br-2 flex flex-col items-center justify-center gap-2 splash-left-col"> */}
+          <div className="splash-image-container flex flex-1">
+              <div className="splash-image-logo-container m-1">
+              </div>
+            {/* <img.splash-logo-container
+              src="./images/logo_name.svg"
+              className="splash-screen-logo pos-abs left-0 top-0 m-2"
+              width="88px"
+              height="100%"
+            ></img> */}
+            {/* <img
+              src="./images/splashmodal_art.webp"
+              width="100%"
+              height="100%"
+            ></img> */}
           </div>
+          {/* </div> */}
 
           {/* right column */}
           <div className="video-container flex-1 flex flex-col gap-2 p-1 splash-right-col">
             {/* row 1 */}
             <div>
-              <p className="text-white font-bold text-lg">
-                Free Motion Capture for Everyone –—
-              </p>
+              <h1 className="title"><span className="color-gray-100">
+                Free Motion Capture for</span><br/><span className="color-gray-400">Everyone –—</span> 
+              </h1>
             </div>
 
             {/* row 2 */}
@@ -76,7 +78,7 @@ const SplashModal: React.FC<SplashModalProps> = ({ onClose }) => {
               <p className="text-white text-sm">Paragraph 2 of row 4</p>
             </div>
           </div>
-        </div>
+          {/* </div> */}
         </div>
       </div>
     </div>

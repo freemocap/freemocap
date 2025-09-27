4219ebe5fc30abd64500b399b78647f381ada54e
Pooya Deperson <poamrd@gmail.com>
2025-09-27 18:37:55 +0200

    adding undercore

--- app-design-v2/src/components/FileDirectorySettingsModal.tsx
+++ app-design-v2/src/components/FileDirectorySettingsModal.tsx
@@ -79,7 +79,7 @@ const FileDirectorySettingsModal: React.FC<FileDirectorySettingsModalProps> = ({
     const hours = String(now.getHours()).padStart(2, "0");
     const minutes = String(now.getMinutes()).padStart(2, "0");
     const seconds = String(now.getSeconds()).padStart(2, "0");
-    return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}\u00A0`;
+    return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}_`;
   };
 
   useEffect(() => {

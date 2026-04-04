c9859a30f539c33889a217b4aac5ed360ccca031
jonmatthis <jonmatthis@gmail.com>
2026-04-04 16:32:06 -0400

    blenderrouterr!

--- freemocap/core/blender/helpers/freemocap_blender_addon_helpers/run_simple.py
+++ freemocap/core/blender/helpers/freemocap_blender_addon_helpers/run_simple.py
@@ -1,7 +1,12 @@
 import sys
 
 
-def run_simple(recording_path_input: str, blender_file_save_path_input: str):
+def run_simple(site_packages_path: str, recording_path_input: str, blender_file_save_path_input: str):
+    # Inject the freemocap venv's site-packages so freemocap_blender_addon is importable
+    # without needing to install the addon into Blender
+    if site_packages_path not in sys.path:
+        sys.path.insert(0, site_packages_path)
+
     from freemocap_blender_addon.main import ajc27_run_as_main_function
 
     ajc27_run_as_main_function(recording_path=str(recording_path_input),
@@ -9,13 +14,15 @@ def run_simple(recording_path_input: str, blender_file_save_path_input: str):
 
 
 if __name__ == "__main__":
-    print(f"\nRunning {__file__} as a subprocess to install the addon...\n")
+    print(f"\nRunning {__file__} as a subprocess...\n")
     argv = sys.argv
     print(f"Received command line arguments: {argv}")
     argv = argv[argv.index("--") + 1:]
-    recording_path_input = str(argv[0])
-    blender_file_save_path_input = str(argv[1])
-    run_simple(recording_path_input=recording_path_input,
+    site_packages_path_input = str(argv[0])
+    recording_path_input = str(argv[1])
+    blender_file_save_path_input = str(argv[2])
+    run_simple(site_packages_path=site_packages_path_input,
+               recording_path_input=recording_path_input,
                blender_file_save_path_input=blender_file_save_path_input)
 
     print("\nDone!\n")

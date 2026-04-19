9262b33ba0ce115e746c3c2984db0c36566e8833
jonmatthis <jonmatthis@gmail.com>
2026-04-19 10:44:16 -0400

    types seem happy enough for now

--- freemocap/core/tasks/mocap/mocap_task_config.py
+++ freemocap/core/tasks/mocap/mocap_task_config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, ConfigDict, Field
-from skellytracker.trackers.base_tracker.detector_helpers import DetectorConfig
+from skellytracker.trackers.base_tracker.detector_helpers import  SkeletonDetectorConfig
 from skellytracker.trackers.rtmpose_tracker.rtmpose_detector import RTMPoseDetectorConfig
 
 from freemocap.core.tasks.calibration.calibration_task_config import CalibrationSource
@@ -10,7 +10,7 @@ from freemocap.core.tasks.calibration.calibration_task_config import Calibration
 class PosthocMocapPipelineConfig(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
-    detector_config: DetectorConfig = Field(default_factory= RTMPoseDetectorConfig)#LegacyMediapipeDetectorConfig)
+    skeleton_detector_config: SkeletonDetectorConfig = Field(default_factory= RTMPoseDetectorConfig)#LegacyMediapipeDetectorConfig)
     calibration_source: CalibrationSource = Field(
         default=CalibrationSource.MOST_RECENT,
         alias="calibrationSource",
@@ -26,11 +26,11 @@ class PosthocMocapPipelineConfig(BaseModel):
     @classmethod
     def default_realtime(cls) -> "PosthocMocapPipelineConfig":
         # return cls(detector_config=RTMPoseDetectorConfig())
-        return cls(detector_config=RTMPoseDetectorConfig())
+        return cls(skeleton_detector_config=RTMPoseDetectorConfig())
 
     @classmethod
     def default_posthoc(cls) -> "PosthocMocapPipelineConfig":
-        return cls(detector_config=RTMPoseDetectorConfig())
+        return cls(skeleton_detector_config=RTMPoseDetectorConfig())
 
     # @classmethod
     # def default_realtime(cls) -> "PosthocMocapPipelineConfig":

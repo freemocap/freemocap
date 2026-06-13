f1188244e30200e68e94367b0fb790ce6433d833
Pooya Deperson <poamrd@gmail.com>
2026-06-13 16:32:12 +0200

    splash modal image and others

--- freemocap-ui/src/components/ui-components/NameDropdownSelector.tsx
+++ freemocap-ui/src/components/ui-components/NameDropdownSelector.tsx
@@ -47,7 +47,7 @@ const NameDropdownSelector: React.FC<NameDropdownSelectorProps> = ({
       </button>
 
       {open && (
-        <div className={`dropdown-container top-30 border-1 border-black elevated-sharp pos-abs flex flex-col right-0 p-1 bg-dark br-2 z-1 reveal slide-down ${DropdownclassName}`}>
+        <div className={`dropdown-container top-30 border-1 min-w-full border-black elevated-sharp pos-abs flex flex-col right-0 p-1 bg-dark br-2 z-1 reveal slide-down ${DropdownclassName}`}>
           <div className="flex flex-col right-0 p-1 gap-2 bg-middark br-1 z-1">
             {options.map((option, index) => (
               <button

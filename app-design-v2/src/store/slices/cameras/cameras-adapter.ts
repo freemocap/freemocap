fb4fce67837a863b6697da690f7583561452429c
jonmatthis <jonmatthis@gmail.com>
2026-06-16 18:49:58 -0400

    rm app-design-v2

--- app-design-v2/src/store/slices/cameras/cameras-adapter.ts
+++
@@ -1,7 +0,0 @@
-import {createEntityAdapter} from '@reduxjs/toolkit';
-import type {CameraDevice} from './cameras-types';
-
-export const cameraAdapter = createEntityAdapter<CameraDevice, string>({
-    selectId: (camera) => camera.cameraId,
-    sortComparer: (a, b) => a.index - b.index,
-});

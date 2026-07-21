8d27c626bf4d2eb73361cf8224a9b87a03e77924
jonmatthis <jonmatthis@gmail.com>
2026-07-21 17:58:48 -0400

    fix versions and stuff, hopefully

--- freemocap/core/__init__.py
+++ freemocap/core/__init__.py
@@ -1,14 +1,5 @@
 """A free and open source markerless motion capture system for everyone 💀✨"""
 
-__author__ = """Skelly FreeMoCap"""
-__email__ = "info@freemocap.org"
-__version__ = "v1.4.7"
-__description__ = "A free and open source markerless motion capture system for everyone 💀✨"
-
-__package_name__ = "freemocap"
-__repo_url__ = f"https://github.com/freemocap/{__package_name__}/"
-__repo_issues_url__ = f"{__repo_url__}issues"
-
 from skellylogs import configure_logging, LogLevels
 
 LOG_LEVEL = LogLevels.TRACE
